a01bdfa0719551a6bcd2087acb713ad5d6b6c86f
meyric <meyric@dxw.com>
2022-06-10 12:47:08 +0100

    Add basic GOVUK page layout
    Here we begin to use the GOVUK design system.
    
    We add partials for the header and footer, update the applicaiton layout
    to include the required elements and add the assets to the asset
    pipeline that we need to use in the view (rather than in the sass
    files).
    
    It is really important to include the `js-enabled` class to the body
    element if we want the GOVUK JS to work later.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,10 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//
+//= link govuk-frontend/govuk/assets/images/favicon.ico
+//= link govuk-frontend/govuk/assets/images/govuk-apple-touch-icon-180x180.png
+//= link govuk-frontend/govuk/assets/images/govuk-mask-icon.svg
+//= link govuk-frontend/govuk/assets/images/govuk-apple-touch-icon-167x167.png
+//= link govuk-frontend/govuk/assets/images/govuk-apple-touch-icon-152x152.png
+//= link govuk-frontend/govuk/assets/images/govuk-apple-touch-icon.png
+//= link govuk-frontend/govuk/assets/images/govuk-opengraph-image.png
