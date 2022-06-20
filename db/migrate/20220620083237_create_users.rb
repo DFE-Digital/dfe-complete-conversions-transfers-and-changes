cd140b329474c33d5e41c48f7b6b4e23cd4a731c
meyric <meyric@dxw.com>
2022-06-20 09:18:28 +0100

    Add User model
    We will store very basic info about users, just enough to sign them in,
    which comes later.
    
    Here, we add a model and table to store them. We'll use their email
    address as this is the only unique identifeir we will know.

---
+++ db/migrate/20220620083237_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    enable_extension "pgcrypto" unless extension_enabled?("pgcrypto")
+
+    create_table :users, id: :uuid do |t|
+      t.string :email, index: { unique: true }
+      t.timestamps
+    end
+  end
+end
