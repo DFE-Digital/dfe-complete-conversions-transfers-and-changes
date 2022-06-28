99e13bfa26249b39312d03a955869f60fc7b391e
Nick Jackson <nick@dxw.com>
2022-06-28 16:34:53 +0100

    Add team_leader boolean field to User model
    The new `team_leader` field on the `User` model records if the `User`
    should be considered a team leader. There are currently no other
    possible roles, so this is a straight boolean which defaults to `false`.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_23_165020) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_28_153154) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_23_165020) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "team_leader", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
