97ba8e7d0ba99c867e7b475c7873090d8a476018
Joe <jkempster34@gmail.com>
2022-07-13 15:02:38 +0100

    Remove duplicated FactoryBot configuration
    We are accidentally adding FactoryBot configuration twice. Remove one of
    the instances.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,7 +67,6 @@ RSpec.configure do |config|
   # Include helpers for tests
   config.include SignInHelpers
   config.include FeatureHelpers, type: :feature
-  config.include FactoryBot::Syntax::Methods
 
   # cleanup Omniauth after each example
   config.after(:each) do |example|
