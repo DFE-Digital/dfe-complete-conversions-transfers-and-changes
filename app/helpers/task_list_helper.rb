69f2e87aec75f7ab9ed905cc2816a7d0c4f9495e
Nick Jackson <nick@dxw.com>
2022-07-22 11:55:32 +0100

    Create new task_status_tag helper
    When given a task, this helper will (eventually) output a GOV.UK-styled
    tag with the correct text, colour and ID. For the moment, this defaults
    to "Completed" so it is at parity with the previous code.

--- app/helpers/task_list_helper.rb
+++ app/helpers/task_list_helper.rb
@@ -2,4 +2,9 @@ module TaskListHelper
   def task_status_id(task)
     "#{task.title.parameterize}-status"
   end
+
+  def task_status_tag(task)
+
+    govuk_tag(text: 'Completed', classes: "app-task-list__tag", html_attributes: {id: task_status_id(task)})
+  end
 end
