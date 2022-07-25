52f1e61c7884b7affb3b22d2872fe61057f30768
Nick Jackson <nick@dxw.com>
2022-07-25 13:09:38 +0100

    Tasks correctly report their status
    Given a number of actions n, of which between 0 and n are completed,
    return the correct completion status of the task.

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,7 +6,21 @@ class Task < ApplicationRecord
 
   delegate :project, to: :section
 
+  def actions_count
+    @actions_count ||= actions.all.count
+  end
+
+  def completed_actions_count
+    @completed_actions_count ||= actions.where(completed: true).count
+  end
+
   def status
-    :unknown
+    if completed_actions_count == 0
+      :not_started
+    elsif completed_actions_count == actions_count
+      :completed
+    elsif completed_actions_count < actions_count
+      :in_progress
+    end
   end
 end
