f3a2846617bc2e1106502e5937122b3de0a9e834
Nick Jackson <nick@dxw.com>
2022-07-25 17:07:57 +0100

    Update details show in project summary
    Instead of the delivery officer (now on the project information tab),
    show the project's target completion date, incoming trust name and local
    authority name as part of the summary information.
    
    This summary block appears at the top of both the task list and project
    information views to assist delivery officers with orientation.

--- spec/factories/academies_api/establishment.rb
+++ spec/factories/academies_api/establishment.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :academies_api_establishment, class: "AcademiesApi::Establishment" do
     name { "Caludon Castle School" }
+    local_authority { "West Placefield Council" }
   end
 end
