d5e66621b6980bab17ec938417cde8270405fe40
meyric <meyric@dxw.com>
2022-08-23 16:01:00 +0100

    Improve the User factory
    We have 3 types of users:
    
    - caseworker
    - team lead
    - regional delivery officer
    
    It makes sense for us to have these as traits in our factory.
    
    We've also changed the filename to be more consistent with the other
    factories.

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -4,12 +4,18 @@ FactoryBot.define do
     team_leader { false }
     regional_delivery_officer { false }
 
+    trait :caseworker do
+      email { "caseworker@education.gov.uk" }
+    end
+
     trait :team_leader do
       team_leader { true }
+      email { "team.lead@education.gov.uk" }
     end
 
     trait :regional_delivery_officer do
       regional_delivery_officer { true }
+      email { "regional.delivery.officer@education.gov.uk" }
     end
   end
 end
