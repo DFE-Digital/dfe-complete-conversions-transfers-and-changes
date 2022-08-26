9e216248851ae223d55c720101c22364ff6eb529
Nick Jackson <nick@dxw.com>
2022-08-26 14:41:00 +0100

    Add naive grouping of contacts
    This groups the contacts on the project page by their category slug.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,6 @@
 class ContactsController < ApplicationController
   before_action :find_project
-  before_action :find_contacts, only: :index
+  before_action :find_grouped_contacts, only: :index
 
   def index
   end
@@ -44,8 +44,8 @@ class ContactsController < ApplicationController
     @project = Project.find(params[:project_id])
   end
 
-  private def find_contacts
-    @contacts = Contact.where(project: @project)
+  private def find_grouped_contacts
+    @contacts = Contact.where(project: @project).group_by(&:category)
   end
 
   private def contact_params
