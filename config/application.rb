aaa6c92c770e8f2aa1209a1a150ddd954c8aed27
meyric <meyric@dxw.com>
2022-08-26 10:54:00 +0100

    Add basic background job support
    We'll use Sidekiq to run our ActiveJobs.
    
    Sidekiq uses Redis so we add that the to the local requirements via the
    Brewfile and a `docker-compose.local.yml` to spin up Redis and the
    Database together.

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,7 @@ require_relative "boot"
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
-# require "active_job/railtie"
+require "active_job/railtie"
 require "active_record/railtie"
 # require "active_storage/engine"
 require "action_controller/railtie"
@@ -35,5 +35,8 @@ module DfeCompleteConversionsTransfersAndChanges
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # ActiveJob uses Sidekiq
+    config.active_job.queue_adapter = :sidekiq
   end
 end
