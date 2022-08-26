a6a4647eb969b0fdf45da49ff6554b4ec4801ff7
Nick Jackson <nick@dxw.com>
2022-08-26 15:58:24 +0100

    Correctly format contact grouping headers
    Slugs from the enum in the database now have underscores replaced with
    spaces, and the first letter capitalised.

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -2,4 +2,8 @@ module ContactsHelper
   def has_contacts?(contacts)
     contacts.present? && contacts.any?
   end
+
+  def format_category_name(category)
+    category.tr("_", " ").capitalize
+  end
 end
