d2a243df2d9f36de5cb10d04789f73207191b401
Joe <jkempster34@gmail.com>
2022-09-05 18:03:02 +0100

    Add optional handover comment field to the new project form
    The handover proforma and KIM allow users to add a handover note. We want to replicate this by adding an optional "handover comment" field to the new project form. This will create a new `Note` record for the `Project`.

--- spec/requests/projects_controller_spec.rb
+++ spec/requests/projects_controller_spec.rb
@@ -9,14 +9,16 @@ RSpec.describe ProjectsController, type: :request do
   end
 
   describe "#create" do
+    let(:project) { build(:project) }
+    let(:project_params) { attributes_for(:project, regional_delivery_officer: nil) }
+    let(:note_params) { {body: "new note"} }
+
     subject(:perform_request) do
-      post projects_path, params: {project: {urn: 123456}}
+      post projects_path, params: {project: {**project_params, note: note_params}}
       response
     end
 
     context "when the project is not valid" do
-      let(:project) { build(:project, team_leader: nil) }
-
       before do
         allow(Project).to receive(:new).and_return(project)
         allow(project).to receive(:valid?).and_return false
@@ -28,18 +30,37 @@ RSpec.describe ProjectsController, type: :request do
     end
 
     context "when the project is valid" do
-      let(:project) { build(:project, team_leader: nil) }
+      let(:task_list_creator) { TaskListCreator.new }
 
       before do
         mock_successful_api_responses(urn: 123456, ukprn: 10061021)
-        allow(Project).to receive(:new).and_return(project)
-        allow(project).to receive(:valid?).and_return(true)
+
+        allow(TaskListCreator).to receive(:new).and_return(task_list_creator)
+        allow(task_list_creator).to receive(:call).and_return true
+
+        perform_request
       end
 
-      it "assigns the team leader, calls the TaskListCreator, and redirects to the project path" do
-        expect_any_instance_of(TaskListCreator).to receive(:call).with(project)
-        expect(perform_request).to redirect_to(project_path(project.id))
-        expect(project.regional_delivery_officer).to eq regional_delivery_officer
+      it "assigns the regional delivery officer, calls the TaskListCreator, and redirects to the project path" do
+        new_project_record = Project.last
+
+        expect(response).to redirect_to(project_path(Project.first.id))
+        expect(task_list_creator).to have_received(:call).with(new_project_record)
+        expect(new_project_record.regional_delivery_officer).to eq regional_delivery_officer
+      end
+
+      it "creates a new project and note" do
+        expect(Project.count).to be 1
+        expect(Note.count).to be 1
+        expect(Note.last.user).to eq regional_delivery_officer
+      end
+
+      context "when the note body is empty" do
+        let(:note_params) { {body: ""} }
+
+        it "does not create a new note" do
+          expect(Note.count).to be 0
+        end
       end
     end
   end
