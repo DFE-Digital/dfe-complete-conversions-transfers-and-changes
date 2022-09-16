3c157308a3184d19f85c18c43f97d948801798be
meyric <meyric@dxw.com>
2022-09-16 12:28:25 +0100

    Allow users to add advisory board data
    Collecting the advisory board date and any conditions are part of the
    handover process (create new projects).
    
    We've added some additional markup context to make testing simpler.

--- spec/features/users_can_create_projects_spec.rb
+++ spec/features/users_can_create_projects_spec.rb
@@ -11,14 +11,27 @@ RSpec.feature "Users can create new projects" do
   context "when the URN and UKPRN are valid" do
     let(:urn) { 123456 }
     let(:ukprn) { 10061021 }
+    let(:two_weeks_ago) { Date.today - 2.weeks }
 
     before { mock_successful_api_responses(urn: urn, ukprn: ukprn) }
 
     scenario "a new project is created" do
       fill_in "School URN", with: urn
       fill_in "Incoming trust UK Provider Reference Number (UKPRN)", with: ukprn
-      fill_in "Month", with: 12
-      fill_in "Year", with: 2025
+
+      within("#target-completion-date") do
+        fill_in "Month", with: 12
+        fill_in "Year", with: 2025
+      end
+
+      within("#advisory-board-date") do
+        fill_in "Day", with: two_weeks_ago.day
+        fill_in "Month", with: two_weeks_ago.month
+        fill_in "Year", with: two_weeks_ago.year
+      end
+
+      fill_in "project-advisory-board-conditions-field", with: "This school must:\n1. Do this\n2. And that"
+
       fill_in "Handover comments", with: "A new handover comment"
 
       click_button("Continue")
@@ -26,6 +39,10 @@ RSpec.feature "Users can create new projects" do
       expect(page).to have_content(I18n.t("project.show.title"))
       expect(page).to have_content("Project kick-off")
       expect(page).to have_content("Handover with regional delivery officer")
+
+      click_on("Project information")
+
+      expect(page).to have_content(two_weeks_ago.to_formatted_s(:govuk))
     end
   end
 end
