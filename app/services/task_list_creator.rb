4503cc539f608217355028fe603aa2af66b654e7
Nick Jackson <nick@dxw.com>
2022-09-20 16:29:01 +0100

    Rearrange TaskListCreator
    This breaks TaskListCreator into smaller functions, as well as renaming
    some existing ones and their arguments so it's more explicit what
    they're doing and what inputs are expected.

--- app/services/task_list_creator.rb
+++ app/services/task_list_creator.rb
@@ -1,39 +1,41 @@
 class TaskListCreator
   def call(project, workflow_root:)
-    workflow = load_workflow_definition(workflow_root)
+    workflow = load_workflow_tasklist_from_root(workflow_root)
 
     workflow.fetch("sections").each_with_index do |section_name, index|
-      section_data = load_workflow_section(workflow_root, section_name)
-
+      section_data = load_workflow_section_from_root(workflow_root, section_name)
       section = Section.create(title: section_data.fetch("title"), order: index, project: project)
-
-      section_data.fetch("tasks").each_with_index do |workflow_task, index|
-        task = Task.create(
-          title: workflow_task.fetch("title"),
-          hint: workflow_task.fetch("hint", nil),
-          guidance_summary: workflow_task.fetch("guidance_summary", nil),
-          guidance_text: workflow_task.fetch("guidance_text", nil),
-          order: index,
-          section: section,
-          optional: workflow_task.fetch("optional", false)
-        )
-
-        create_actions(workflow_task, task)
-      end
+      create_tasks_in_section(section_data, section)
     end
   end
 
-  private def load_workflow_definition(workflow_root)
+  private def load_workflow_tasklist_from_root(workflow_root)
     YAML.load_file(File.join(workflow_root, "tasklist.yml"))
   end
 
-  private def load_workflow_section(workflow_root, section)
+  private def load_workflow_section_from_root(workflow_root, section)
     YAML.load_file(Rails.root.join(workflow_root, "sections", "#{section}.yml"))
   end
 
-  private def create_actions(workflow_task, task)
-    workflow_task.fetch("actions").each_with_index do |workflow_action, index|
-      Action.create(workflow_action.merge({order: index, task_id: task.id}))
+  private def create_tasks_in_section(section_data, section)
+    section_data.fetch("tasks").each_with_index do |task_data, index|
+      task = Task.create(
+        title: task_data.fetch("title"),
+        hint: task_data.fetch("hint", nil),
+        guidance_summary: task_data.fetch("guidance_summary", nil),
+        guidance_text: task_data.fetch("guidance_text", nil),
+        order: index,
+        section: section,
+        optional: task_data.fetch("optional", false)
+      )
+
+      create_actions_in_task(task_data, task)
+    end
+  end
+
+  private def create_actions_in_task(task_data, task)
+    task_data.fetch("actions").each_with_index do |action_data, index|
+      Action.create(action_data.merge({order: index, task_id: task.id}))
     end
   end
 end
