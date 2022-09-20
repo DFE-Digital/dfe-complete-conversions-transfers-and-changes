89434f43f2c8cf1e13d85fb649e651760cfc7ab0
Nick Jackson <nick@dxw.com>
2022-09-20 16:10:54 +0100

    Titleise the trust name for display
    Previously trust names would display in ALL CAPS, since this is how they
    arrive from upstream APIs. They are now run through `titleize` before
    display, which converts them to title case.

--- spec/factories/academies_api/trust.rb
+++ spec/factories/academies_api/trust.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :academies_api_trust, class: "AcademiesApi::Trust" do
-    name { "THE ROMERO CATHOLIC ACADEMY" }
+    original_name { "THE ROMERO CATHOLIC ACADEMY" }
     companies_house_number { "09702162" }
   end
 end
