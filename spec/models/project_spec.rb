6024ebe4c8f4f89814a3e5dc02d52c7dcae57cad
meyric <meyric@dxw.com>
2022-10-17 14:12:27 +0100

    Add scope for ordering by target completion date
    This seems the most reasonable sort order in a lot of situations and is
    certainly an improvement over a random order from the database, which is
    what we have now.
    
    Here we create the scope but do not apply it, that comes next.

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -266,6 +266,22 @@ RSpec.describe Project, type: :model do
     end
   end
 
+  describe "by_target_completion_date scope" do
+    before { mock_successful_api_responses(urn: any_args, ukprn: any_args) }
+
+    it "shows the project that complete earliest first" do
+      last_project = create(:project, target_completion_date: Date.today.beginning_of_month + 3.years)
+      middle_project = create(:project, target_completion_date: Date.today.beginning_of_month + 2.years)
+      first_project = create(:project, target_completion_date: Date.today.beginning_of_month + 1.year)
+
+      ordered_projects = Project.by_target_completion_date
+
+      expect(ordered_projects[0]).to eq first_project
+      expect(ordered_projects[1]).to eq middle_project
+      expect(ordered_projects[2]).to eq last_project
+    end
+  end
+
   describe "#closed?" do
     context "when the closed_at is nil, i.e. the project is active" do
       it "returns false" do
