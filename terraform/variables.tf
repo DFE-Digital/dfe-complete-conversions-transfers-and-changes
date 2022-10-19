102a79f56cef1010e9e48d07bb8f5e1c412a4a15
Chris Wright <chris@dxw.com>
2022-10-19 12:45:11 +0100

    Add list of Key Vault users
    * Allows providing a list of users that require access to the Key Vault
      tfvars secrets
    * The values should be the User Principle Name found in AD

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,6 +3,11 @@ variable "environment" {
   type        = string
 }
 
+variable "key_vault_access_users" {
+  description = "List of users that require access to the Key Vault where tfvars are stored. This should be a list of User Principle Names (Found in Active Directory) that need to run terraform"
+  type        = list(string)
+}
+
 variable "tfvars_filename" {
   description = "tfvars filename. This file is uploaded and stored encrupted within Key Vault, to ensure that the latest tfvars are stored in a shared place."
   type        = string
