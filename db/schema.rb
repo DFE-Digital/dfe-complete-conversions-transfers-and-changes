8f4f70d5be55fba1ba2b916d456090882caf0ed1
Nick Jackson <nick@dxw.com>
2022-10-19 14:16:03 +0100

    Add new database column to store task slugs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_12_123642) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_18_122208) do
   create_table "actions", id: :uuid, default: -> { "newid()" }, force: :cascade do |t|
     t.string "title", null: false
     t.integer "order", null: false
@@ -91,6 +91,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_12_123642) do
     t.text "guidance_text"
     t.boolean "optional"
     t.boolean "not_applicable", default: false
+    t.string "slug"
     t.index ["section_id"], name: "index_tasks_on_section_id"
   end
 
