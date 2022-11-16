48baf0e9ef8f2ab611885e61bc86e4705b652289
Laura Porter <laura@dxw.com>
2022-11-16 12:10:07 +0000

    Change remaining i18n strings to `provisional_completion_date`
    We are renaming the `target_completion_date` column on Projects to
    `provisional_completion_date`, and therefore renaming the i18n keys and
    their strings to say "provisional" instead of "target".

--- spec/features/users_can_view_projects_spec.rb
+++ spec/features/users_can_view_projects_spec.rb
@@ -127,7 +127,7 @@ RSpec.feature "Users can view a list of projects" do
 
     within urn.ancestor("li") do
       expect(page).to have_content("School type: #{project.establishment.type}")
-      expect(page).to have_content("Target conversion date: #{project.provisional_completion_date.to_formatted_s(:govuk)}")
+      expect(page).to have_content("Provisional conversion date: #{project.provisional_completion_date.to_formatted_s(:govuk)}")
       expect(page).to have_content("Incoming trust: #{project.incoming_trust.name}")
       expect(page).to have_content("Local authority: #{project.establishment.local_authority}")
     end
