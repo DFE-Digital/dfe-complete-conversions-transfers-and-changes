b017e8ec74db196fd05817ef4d70fde6efdd5e1e
Laura Porter <laura@dxw.com>
2022-12-01 12:40:10 +0000

    Only test with Conversion projects in the test suite
    From now on, all projects we create in the app will be Conversion projects.
    Until we add more types of projects, conversion projects will be the default.
    We do not want to run tests with "plain" projects as they aren't used directly
    by any of the application functions.
    
    Change the project factory to only create Conversion::Projects.
    
    The change in `spec/models/project_spec.rb:284` is necessary because when we
    call `Project#index` the projects returned will be Project objects, while the
    projects created by the project factory will be Conversion::Projects. Due to
    STI these are the same type of objects, but with different class names. We have
    amended this test to check the returned objects have the same attributes.
    
    Additionally, move the Project factory into a `conversion` folder

---
+++ spec/factories/conversion/project_factory.rb
@@ -0,0 +1,23 @@
+FactoryBot.define do
+  factory :conversion_project, class: "Project" do
+    type { "Conversion::Project" }
+    urn { 123456 }
+    incoming_trust_ukprn { 10061021 }
+    provisional_conversion_date { (Date.today + 2.years).at_beginning_of_month }
+    team_leader { association :user, :team_leader, email: "team-leader-#{SecureRandom.uuid}@education.gov.uk" }
+    regional_delivery_officer { association :user, :regional_delivery_officer, email: "regional-delivery-officer-#{SecureRandom.uuid}@education.gov.uk" }
+    advisory_board_date { (Date.today - 2.weeks) }
+    establishment_sharepoint_link { "https://educationgovuk-my.sharepoint.com/establishment-folder" }
+    trust_sharepoint_link { "https://educationgovuk-my.sharepoint.com/trust-folder" }
+
+    trait :with_conditions do
+      advisory_board_conditions { "The following must be met:\n 1. Must be red\n2. Must be blue\n" }
+    end
+
+    trait :without_any_assigned_roles do
+      team_leader { nil }
+      regional_delivery_officer { nil }
+      caseworker { nil }
+    end
+  end
+end
