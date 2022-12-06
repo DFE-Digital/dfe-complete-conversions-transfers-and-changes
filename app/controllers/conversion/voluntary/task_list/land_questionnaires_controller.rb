30b202aaf26b99ede00946c7d9cb40560b1dfcc3
Joe <jkempster34@gmail.com>
2022-12-06 16:58:11 +0000

    DRY up controllers a bit

--- app/controllers/conversion/voluntary/task_list/land_questionnaires_controller.rb
+++ app/controllers/conversion/voluntary/task_list/land_questionnaires_controller.rb
@@ -1,17 +1,10 @@
 class Conversion::Voluntary::TaskList::LandQuestionnairesController < Conversion::Voluntary::TaskList::BaseTasksController
   def edit
-    @project = Project.find(params[:project_id])
-    # @land_questionnaire = @project.task_list.land_questionnaire
-    # Why doesn't this work? >:(
-
-    @land_questionnaire = Conversion::Voluntary::TaskList.find_by(project: @project).land_questionnaire
-    # @tasks = Conversion::Voluntary::TaskList.find_by(project: @project).tasks
+    @land_questionnaire = @task_list.land_questionnaire
   end
 
   def update
     @land_questionnaire = Conversion::Voluntary::LandQuestionnaire.new(land_questionnaire_params)
-    @project = Project.find(params[:project_id])
-    @task_list = Conversion::Voluntary::TaskList.find_by(project: @project)
 
     if @land_questionnaire.valid?
       @task_list.update!(land_questionnaire: @land_questionnaire)
@@ -23,10 +16,6 @@ class Conversion::Voluntary::TaskList::LandQuestionnairesController < Conversion
   end
 
   private
-
-  def task_list
-    Conversion::Voluntary::TaskList.find_by(project: @project)
-  end
 
   def land_questionnaire_params
     params.require(:conversion_voluntary_land_questionnaire).permit \
