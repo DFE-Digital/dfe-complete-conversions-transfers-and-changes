26ffd29db3f7abdb3279cedf275608f1f223441f
Joe <jkempster34@gmail.com>
2022-12-06 15:26:45 +0000

    Dynamically map Tasks to TaskList

--- app/models/conversion/voluntary/task_list.rb
+++ app/models/conversion/voluntary/task_list.rb
@@ -1,14 +1,21 @@
 class Conversion::Voluntary::TaskList < ApplicationRecord
   belongs_to :project, inverse_of: :task_list
 
-  composed_of \
-    :land_questionnaire,
-    class_name: "Conversion::Voluntary::LandQuestionnaire",
-    mapping: [
-      %w[land_questionnaire_received received],
-      %w[land_questionnaire_cleared cleared],
-      %w[land_questionnaire_signed_by_solicitor signed_by_solicitor],
-      %w[land_questionnaire_saved_in_school_sharepoint saved_in_school_sharepoint]
-    ],
-    constructor: :compose
+  Tasks = [
+    Conversion::Voluntary::LandQuestionnaire
+  ].freeze
+
+  Tasks.each do |task|
+    task_instance = task.new
+
+    mapping = task_instance.attributes.keys.map do |attribute|
+      ["#{task.key}_#{attribute}", attribute]
+    end
+
+    composed_of \
+      task.key.to_sym,
+      class_name: task.name,
+      mapping: mapping,
+      constructor: :compose
+  end
 end
