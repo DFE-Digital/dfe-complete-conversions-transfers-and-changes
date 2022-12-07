4367f84c9f02dc7af6d4a563f55191b89c75f307
Laura Porter <laura@dxw.com>
2022-12-07 10:57:19 +0000

    Use one Factory with inheritance for the FormObjects
    DRY up the FormObject specs even more with some Factorybot inheritance.

--- app/forms/conversion/voluntary/create_project_form.rb
+++ app/forms/conversion/voluntary/create_project_form.rb
@@ -1,36 +1,4 @@
-class Conversion::Voluntary::CreateProjectForm
-  include ActiveModel::Model
-  include ActiveRecord::AttributeAssignment
-
-  SHAREPOINT_URLS = %w[educationgovuk-my.sharepoint.com educationgovuk.sharepoint.com].freeze
-
-  attr_accessor :urn,
-    :incoming_trust_ukprn,
-    :establishment_sharepoint_link,
-    :trust_sharepoint_link,
-    :advisory_board_conditions,
-    :note_body,
-    :user,
-    :provisional_conversion_date,
-    :advisory_board_date
-
-  validates :urn,
-    :incoming_trust_ukprn,
-    :establishment_sharepoint_link,
-    :trust_sharepoint_link,
-    :advisory_board_date,
-    :provisional_conversion_date, presence: true
-
-  validates :urn, urn: true
-  validates :incoming_trust_ukprn, ukprn: true
-
-  validates :provisional_conversion_date, date_in_the_future: true
-  validates :provisional_conversion_date, first_day_of_month: true
-  validates :advisory_board_date, date_in_the_past: true
-
-  validates :establishment_sharepoint_link, presence: true, url: {hostnames: SHAREPOINT_URLS}
-  validates :trust_sharepoint_link, presence: true, url: {hostnames: SHAREPOINT_URLS}
-
+class Conversion::Voluntary::CreateProjectForm < Conversion::CreateProjectForm
   def save
     @project = Conversion::Project.new(
       urn: urn,
--- spec/factories/conversion/create_project_form_factory.rb
+++ spec/factories/conversion/create_project_form_factory.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :create_voluntary_project_form, class: "Conversion::Voluntary::CreateProjectForm" do
+  factory :create_project_form, class: "Conversion::CreateProjectForm" do
     urn { 123456 }
     incoming_trust_ukprn { 10061021 }
     provisional_conversion_date { (Date.today + 2.years).at_beginning_of_month }
@@ -7,5 +7,11 @@ FactoryBot.define do
     establishment_sharepoint_link { "https://educationgovuk-my.sharepoint.com/establishment-folder" }
     trust_sharepoint_link { "https://educationgovuk-my.sharepoint.com/trust-folder" }
     user { association :user, :regional_delivery_officer }
+  end
+
+  factory :create_involuntary_project_form, parent: :create_project_form, class: "Conversion::Involuntary::CreateProjectForm" do
+  end
+
+  factory :create_voluntary_project_form, parent: :create_project_form, class: "Conversion::Voluntary::CreateProjectForm" do
   end
 end
