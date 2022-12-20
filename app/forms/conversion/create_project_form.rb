9f49b20d244153d7249fdcfa20fd4ed2083a6168
Laura Porter <laura@dxw.com>
2022-12-20 10:39:25 +0000

    Add trust_exists & establishment_exists validations
    Add the trust_exists & establishment_exists validations to the FormObject.
    
    With help from https://github.com/DFE-Digital/dfe-complete-conversions-transfers-and-changes/pull/303/
    
    This adds validations to the FormObject which check if the trust & school
    actually exist, before creating the project. It also tightens up some of the
    existing validations, and corrects a potential false positive in the existing
    tests for `urn` and `ukprn`.

--- app/forms/conversion/create_project_form.rb
+++ app/forms/conversion/create_project_form.rb
@@ -1,41 +1,41 @@
 class Conversion::CreateProjectForm
   include ActiveModel::Model
+  include ActiveModel::Attributes
   include ActiveRecord::AttributeAssignment
 
   SHAREPOINT_URLS = %w[educationgovuk-my.sharepoint.com educationgovuk.sharepoint.com].freeze
 
   class NegativeValueError < StandardError; end
 
-  attr_accessor :urn,
-    :incoming_trust_ukprn,
-    :establishment_sharepoint_link,
-    :trust_sharepoint_link,
-    :advisory_board_conditions,
-    :note_body,
-    :user
+  attribute :urn, :integer
+  attribute :incoming_trust_ukprn, :integer
+
+  attr_accessor :establishment_sharepoint_link,
+  :trust_sharepoint_link,
+  :advisory_board_conditions,
+  :note_body,
+  :user
 
   attr_reader :provisional_conversion_date,
     :advisory_board_date
 
-  validates :urn,
-    :incoming_trust_ukprn,
-    :establishment_sharepoint_link,
-    :trust_sharepoint_link,
-    :provisional_conversion_date,
+  validates :provisional_conversion_date,
     :advisory_board_date,
     presence: true
 
-  validates :urn, urn: true
-  validates :incoming_trust_ukprn, ukprn: true
-
-  validates :provisional_conversion_date, date_in_the_future: true
-  validates :provisional_conversion_date, first_day_of_month: true
+  validates :provisional_conversion_date, date_in_the_future: true, first_day_of_month: true
   validates :advisory_board_date, date_in_the_past: true
 
   validates :establishment_sharepoint_link, presence: true, url: {hostnames: SHAREPOINT_URLS}
   validates :trust_sharepoint_link, presence: true, url: {hostnames: SHAREPOINT_URLS}
 
+  validates :urn, presence: true, urn: true
+  validates :incoming_trust_ukprn, presence: true, ukprn: true
+
   validate :multiparameter_date_attributes_values
+
+  validate :establishment_exists, if: -> { urn.present? }
+  validate :trust_exists, if: -> { incoming_trust_ukprn.present? }
 
   def initialize(params = {})
     @attributes_with_invalid_values = []
@@ -73,5 +73,19 @@ class Conversion::CreateProjectForm
     User.team_leaders.each do |team_leader|
       TeamLeaderMailer.new_project_created(team_leader, project).deliver_later
     end
+  end
+
+  private def establishment_exists
+    result = AcademiesApi::Client.new.get_establishment(urn)
+    raise result.error if result.error.present?
+  rescue AcademiesApi::Client::NotFoundError
+    errors.add(:urn, :no_establishment_found)
+  end
+
+  private def trust_exists
+    result = AcademiesApi::Client.new.get_trust(incoming_trust_ukprn)
+    raise result.error if result.error.present?
+  rescue AcademiesApi::Client::NotFoundError
+    errors.add(:incoming_trust_ukprn, :no_trust_found)
   end
 end
