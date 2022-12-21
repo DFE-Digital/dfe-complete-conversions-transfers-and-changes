77b090b4b3cb1183dc274c42351c137f1a8d5f89
Laura Porter <laura@dxw.com>
2022-12-21 11:11:34 +0000

    Add a basic test for the TaskList::Header ViewComponent

---
+++ spec/components/task_header_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe TaskList::TaskHeader, type: :component do
+  let(:project) { create(:conversion_project) }
+  let(:task) { double(status: :not_started, locales_path: "conversion.voluntary.tasks.handover") }
+
+  before { mock_successful_api_responses(urn: any_args, ukprn: any_args) }
+
+  it "renders the title component" do
+    render_inline(TaskList::TaskHeader.new(project: project, task: task))
+
+    expect(page).to have_text(project.establishment.name)
+    expect(page).to have_text("Handover with regional delivery officer")
+  end
+end
