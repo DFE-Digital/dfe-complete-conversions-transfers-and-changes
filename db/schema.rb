5eae5fb005ebfc643c03b61ebca3be12b41ff1a0
meyric <meyric@dxw.com>
2023-01-03 17:43:17 +0000

    Add single worksheet task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_03_161022) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_03_165658) do
   create_table "actions", id: :uuid, default: -> { "newid()" }, force: :cascade do |t|
     t.string "title", null: false
     t.integer "order", null: false
@@ -119,6 +119,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_03_161022) do
     t.boolean "direction_to_transfer_sent"
     t.boolean "direction_to_transfer_signed_secretary_state"
     t.boolean "check_baseline_confirm"
+    t.boolean "single_worksheet_complete"
+    t.boolean "single_worksheet_approve"
+    t.boolean "single_worksheet_send"
   end
 
   create_table "notes", id: :uuid, default: -> { "newid()" }, force: :cascade do |t|
