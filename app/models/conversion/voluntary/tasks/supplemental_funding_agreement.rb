abf01f6333f5d601108b9c6105754a967833556f
meyric <meyric@dxw.com>
2023-01-03 12:26:20 +0000

    Add supplemental funding agreement task

---
+++ app/models/conversion/voluntary/tasks/supplemental_funding_agreement.rb
@@ -0,0 +1,9 @@
+class Conversion::Voluntary::Tasks::SupplementalFundingAgreement < TaskList::Task
+  attribute :received
+  attribute :cleared
+  attribute :signed
+  attribute :saved
+
+  attribute :sent
+  attribute :signed_secretary_state
+end
