760fa05568fd4a7e484a8cd4b614912538a9437e
meyric <meyric@dxw.com>
2023-01-10 09:17:13 +0000

    Redirect to 404 page if project cannot be found
    As we introduce more types of projects there will be instances when a
    project exists but cannot be found. This is because we scope the
    projects down to a subset and then find the id.
    
    For example, a voluntary project exists but should not be shown on an
    involuntary path.
    
    By adding `rescue_from` to the base `projects_controller` we get this
    behaviour in all subclasses.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,7 @@
 class ProjectsController < ApplicationController
   after_action :verify_authorized
   after_action :verify_policy_scoped, only: :index
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_error
 
   def index
     authorize Project
@@ -18,5 +19,9 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.includes(sections: [:tasks]).find(params[:id])
     authorize @project
+  end
+
+  private def not_found_error
+    redirect_to "/404", status: :not_found
   end
 end
