0f78e2cc4bb0ce42c7f3b725c58c5c7a181e0942
meyric <meyric@dxw.com>
2023-01-11 16:26:43 +0000

    Add a not found error to the base controller
    Lots of controllers might need to render a not found error so putting
    this here gives them all access should they need it.
    
    We remove the incorrect version of this from the projects_controller as
    it can use this new one.
    
    We also switched to 'inline private' definitions as this is consistent
    across the code base.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,14 +8,16 @@ class ApplicationController < ActionController::Base
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   rescue_from AcademiesApi::Client::Error, with: :client_error
 
-  private
-
-  def user_not_authorized
+  private def user_not_authorized
     flash[:alert] = I18n.t("unauthorised_action.message")
     redirect_back(fallback_location: root_path)
   end
 
-  def client_error
+  private def client_error
     render "pages/api_client_timeout", status: 500
+  end
+
+  private def not_found_error
+    render "pages/page_not_found", status: :not_found
   end
 end
