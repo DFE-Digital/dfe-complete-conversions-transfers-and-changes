4529be16a91f46d8668968e840691125bdb59263
meyric <meyric@dxw.com>
2023-01-13 15:11:38 +0000

    Add organisation name to contacts
    We learnt that user would find it useful to be able to add an
    organisation name to a contact.
    
    Here we introduce the database and model elements to do so.

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Contact, type: :model do
     it { is_expected.to have_db_column(:title).of_type :string }
     it { is_expected.to have_db_column(:email).of_type :string }
     it { is_expected.to have_db_column(:phone).of_type :string }
+    it { is_expected.to have_db_column(:organisation_name).of_type :string }
   end
 
   describe "Relationships" do
