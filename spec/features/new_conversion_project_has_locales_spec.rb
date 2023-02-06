37baa1589058dd0195fff8e26e06e0b45b2d6b84
Lorna <lorna@dxw.com>
2023-02-06 14:45:43 +0000

    Update hint text and error message for provisional conversion date

---
+++ spec/features/new_conversion_project_has_locales_spec.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+RSpec.feature "All new conversion projects have a locale file & all keys are present" do
+  let(:user) { create(:user, :regional_delivery_officer) }
+
+  before do
+    mock_successful_api_responses(urn: any_args, ukprn: any_args)
+    sign_in_with_user(user)
+  end
+
+  context "voluntary projects" do
+    it "have locales when creating a new project" do
+      visit new_conversions_voluntary_project_path
+
+      expect(page).to_not have_css(".translation_missing")
+    end
+  end
+
+  context "involuntary projects" do
+    it "have locales when creating a new project" do
+      visit new_conversions_voluntary_project_path
+
+      expect(page).to_not have_css(".translation_missing")
+    end
+  end
+end
