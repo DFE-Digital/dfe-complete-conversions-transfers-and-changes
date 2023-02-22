8cb957d729c7923492314109d0d8c3600ab262b3
Lorna <lorna@dxw.com>
2023-02-22 16:06:50 +0000

    Refactor link to school on GIAS
    This refactor was relevant to this work as the trust implementation
    mirrors this school link. It felt logical to refactor this first before
    adding the trust link to GIAS to the project information tab.

--- app/helpers/project_helper.rb
+++ app/helpers/project_helper.rb
@@ -25,4 +25,10 @@ module ProjectHelper
 
     "#{date} #{tag}".html_safe
   end
+
+  def link_to_school_on_gias(urn)
+    raise ArgumentError if urn.nil?
+
+    link_to(t("project_information.show.school_details.rows.view_in_gias"), "https://get-information-schools.service.gov.uk/Establishments/Establishment/Details/#{urn}", target: :_blank)
+  end
 end
