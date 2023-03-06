79336c423c207ac36db934e917b9301b40615158
Lorna <lorna@dxw.com>
2023-03-06 10:42:49 +0000

    Add email address validation to the user model
    There was a user within the system which had an invalid email address,
    which led to adding in a validation for email addresses.
    We added the error handling within the user importer when the user is
    created.
    This is raising a custom InvalidEntryError error which will catch any
    error.
    
    Co-authored-by: Laura <laura@dxw.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,8 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
 
+  validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}
+
   def full_name
     "#{first_name} #{last_name}"
   end
