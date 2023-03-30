f5bf95b6147961048490a76122f113ff36968862
Laura Porter <laura@dxw.com>
2023-03-30 14:12:15 +0100

    Migrate existing Projects to update their region

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_29_140137) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_30_125747) do
   create_table "contacts", id: :uuid, default: -> { "newid()" }, force: :cascade do |t|
     t.uuid "project_id"
     t.string "name", null: false
