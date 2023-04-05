df3c6d6bae1ed24c1baa83da52f997bbf7584527
Lorna <lorna@dxw.com>
2023-04-05 14:40:41 +0100

    Display statistics for projects not assigned with regional casework services

--- app/models/project_statistics.rb
+++ app/models/project_statistics.rb
@@ -42,4 +42,24 @@ class ProjectStatistics
   def completed_projects_with_regional_casework_services
     @projects.assigned_to_regional_caseworker_team.completed.count
   end
+
+  def total_projects_not_with_regional_casework_services
+    @projects.not_assigned_to_regional_caseworker_team.count
+  end
+
+  def voluntary_projects_not_with_regional_casework_services
+    @projects.not_assigned_to_regional_caseworker_team.voluntary.count
+  end
+
+  def sponsored_projects_not_with_regional_casework_services
+    @projects.not_assigned_to_regional_caseworker_team.sponsored.count
+  end
+
+  def in_progress_projects_not_with_regional_casework_services
+    @projects.not_assigned_to_regional_caseworker_team.in_progress.count
+  end
+
+  def completed_projects_not_with_regional_casework_services
+    @projects.not_assigned_to_regional_caseworker_team.completed.count
+  end
 end
--- spec/models/project_statistics_spec.rb
+++ spec/models/project_statistics_spec.rb
@@ -77,4 +77,41 @@ RSpec.describe ProjectStatistics, type: :model do
       end
     end
   end
+
+  describe "Regional projects that are not with regional casework services" do
+    let!(:voluntary_in_progress_project_not_with_regional_casework_services_1) { create(:conversion_project, assigned_to_regional_caseworker_team: false, completed_at: nil) }
+    let!(:voluntary_completed_project_not_with_regional_casework_services_2) { create(:conversion_project, assigned_to_regional_caseworker_team: false, completed_at: Date.today + 2.years) }
+    let!(:sponsored_in_progress_project_not_with_regional_casework_services_1) { create(:involuntary_conversion_project, assigned_to_regional_caseworker_team: false, completed_at: nil) }
+    let!(:sponsored_completed_project_not_with_regional_casework_services_2) { create(:involuntary_conversion_project, assigned_to_regional_caseworker_team: false, completed_at: Date.today + 2.years) }
+
+    describe "#total_projects_not_with_regional_casework_services" do
+      it "returns the total number of projects not with regional casework services" do
+        expect(subject.total_projects_not_with_regional_casework_services).to eql(4)
+      end
+    end
+
+    describe "#voluntary_projects_not_with_regional_casework_services" do
+      it "returns the total number of voluntary projects not with regional casework services" do
+        expect(subject.voluntary_projects_not_with_regional_casework_services).to eql(2)
+      end
+    end
+
+    describe "#sponsored_projects_not_with_regional_casework_services" do
+      it "returns the total number of sponsored projects not with regional casework services" do
+        expect(subject.sponsored_projects_not_with_regional_casework_services).to eql(2)
+      end
+    end
+
+    describe "#in_progress_projects_not_with_regional_casework_services" do
+      it "returns the total number of in-progress projects not with regional casework services" do
+        expect(subject.in_progress_projects_not_with_regional_casework_services).to eql(2)
+      end
+    end
+
+    describe "#completed_projects_not_with_regional_casework_services" do
+      it "returns the total number of completed projects not with regional casework services" do
+        expect(subject.completed_projects_not_with_regional_casework_services).to eql(2)
+      end
+    end
+  end
 end
