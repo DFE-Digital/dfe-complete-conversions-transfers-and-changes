4253fd6bc5474350882d310c109229717464afc5
Laura Porter <laura@dxw.com>
2023-04-11 17:08:56 +0100

    Basic constituency search and member id endpoints

--- app/models/members_api/client.rb
+++ app/models/members_api/client.rb
@@ -1,8 +1,44 @@
 class MembersApi::Client
+  class Error < StandardError; end
+
+  class MultipleResultsError < StandardError; end
+
   attr_reader :connection
 
   def initialize(connection: nil)
     @connection = connection || default_connection
+  end
+
+  def constituency(search_term)
+    response = constituency_search(search_term)
+
+    case response.status
+    when 200
+      body = JSON.parse(response.body)
+      Result.new(body, nil)
+    else
+      Result.new(nil, Error.new(I18n.t("members_api.errors.other")))
+    end
+  end
+
+  def member_id(search_term)
+    constituency_data = constituency(search_term)
+    if constituency_data.object["items"].count > 1
+      Result.new(nil, MultipleResultsError.new(I18n.t("members_api.errors.multiple", search_term: search_term)))
+    else
+      member_id_from_constituency(constituency_data)
+    end
+  end
+
+  private def constituency_search(search_term)
+    @connection.get("/api/Location/Constituency/Search", {searchText: search_term})
+  rescue Faraday::Error => error
+    raise Error.new(error)
+  end
+
+  private def member_id_from_constituency(constituency_data)
+    constituency = constituency_data.object["items"][0]["value"]
+    constituency.dig("currentRepresentation", "member", "value", "id")
   end
 
   private def default_connection
@@ -15,5 +51,14 @@ class MembersApi::Client
         "Content-Type": "application/json"
       }
     )
+  end
+
+  class Result
+    attr_reader :object, :error
+
+    def initialize(object, error)
+      @object = object
+      @error = error
+    end
   end
 end
--- spec/models/members_api/client_spec.rb
+++ spec/models/members_api/client_spec.rb
@@ -11,4 +11,101 @@ RSpec.describe MembersApi::Client do
       expect(client_connection.host).to eql "members-api.test"
     end
   end
+
+  describe "#constituency_search" do
+    let(:client) { described_class.new(connection: fake_successful_constituency_search_connection(fake_response)) }
+
+    subject { client.constituency("St Albans") }
+
+    context "when there is a result" do
+      let(:fake_response) { [200, nil, {items: [{value: {name: "St Albans", id: 12345}}]}.to_json] }
+
+      it "returns a Result with the JSON response body and no error" do
+        expect(subject.object).to eql("items" => [{"value" => {"name" => "St Albans", "id" => 12345}}])
+        expect(subject.error).to be_nil
+      end
+    end
+
+    context "when there is any other error" do
+      let(:fake_response) { [500, nil, nil] }
+
+      it "returns a Result with an Error" do
+        expect(subject.object).to be_nil
+        expect(subject.error).to be_a(MembersApi::Client::Error)
+        expect(subject.error.message).to eq(I18n.t("members_api.errors.other"))
+      end
+    end
+
+    context "when the connection fails" do
+      let(:client) { described_class.new(connection: fake_failed_constituency_search_connection) }
+
+      it "raises an Error" do
+        expect { subject }.to raise_error(MembersApi::Client::Error)
+      end
+    end
+  end
+
+  describe "#member_id" do
+    let(:client) { described_class.new(connection: fake_successful_constituency_search_connection(fake_response)) }
+
+    context "when there is one result" do
+      subject { client.member_id("St Albans") }
+
+      let(:fake_response) do
+        [
+          200,
+          nil,
+          {
+            items: [{
+              value: {
+                name: "St Albans", currentRepresentation: {
+                  member: {
+                    value: {
+                      id: 12345
+                    }
+                  }
+                }
+              }
+            }]
+          }.to_json
+        ]
+      end
+
+      it "returns a the ID" do
+        expect(subject).to eql(12345)
+      end
+    end
+
+    context "when there is more than one result" do
+      subject { client.member_id("St A") }
+
+      let(:fake_response) { [200, nil, {items: [{value: {name: "St Albans"}}, {value: {name: "Lewisham West and Penge"}}]}.to_json] }
+
+      it "returns a MultipleResultsError" do
+        expect(subject.object).to be_nil
+        expect(subject.error).to be_a(MembersApi::Client::MultipleResultsError)
+        expect(subject.error.message).to eq(I18n.t("members_api.errors.multiple", search_term: "St A"))
+      end
+    end
+  end
+
+  def fake_successful_constituency_search_connection(response)
+    Faraday.new do |builder|
+      builder.adapter :test do |stub|
+        stub.get("/api/Location/Constituency/Search") do |_env|
+          response
+        end
+      end
+    end
+  end
+
+  def fake_failed_constituency_search_connection
+    Faraday.new do |builder|
+      builder.adapter :test do |stub|
+        stub.get("/api/Location/Constituency/Search") do |_env|
+          raise Faraday::Error
+        end
+      end
+    end
+  end
 end
