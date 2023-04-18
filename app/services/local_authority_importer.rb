230479693e95b87785da6563b1bc50671192bca8
Lorna <lorna@dxw.com>
2023-04-18 17:56:12 +0100

    Create local authority importer
    We currently have a csv file which has data pulled from KIM. The data
    has been consolidated from the Academy Order Address Details
    spreadsheet.
    
    We followed the existing pattern used for importing users from a csv.
    
    Westminster Council's postcode was incomplete which I fixed within the
    csv. I changed it from SW1E QP to SW1E 6QP.

---
+++ app/services/local_authority_importer.rb
@@ -0,0 +1,37 @@
+require "csv"
+
+class InvalidEntryError < StandardError
+end
+
+class LocalAuthorityImporter
+  def call(csv_path)
+    sanitized_csv(csv_path).each do |row|
+      LocalAuthority.transaction do
+        local_authority = LocalAuthority.find_or_create_by(code: row[:la])
+        local_authority.name = row[:local_authority_name]
+        local_authority.address_1 = row[:dcs_address_1]
+        local_authority.address_2 = row[:dcs_address_2]
+        local_authority.address_3 = row[:dcs_address_3]
+        local_authority.address_town = row[:dcs_town]
+        local_authority.address_county = row[:dcs_county]
+        local_authority.address_postcode = row[:dcs_postcode]
+        unless local_authority.save
+          puts "Unable to save local authority for code #{row[:la]}"
+          raise InvalidEntryError
+        end
+      end
+    end
+  end
+
+  def sanitized_csv(csv_path)
+    sanitized_rows = []
+    CSV.foreach(csv_path, headers: true, header_converters: :symbol) do |row|
+      sanitized_row = row.map do |key, value|
+        value = (value.blank? || value == "0") ? nil : value
+        [key, value]
+      end.to_h
+      sanitized_rows << sanitized_row
+    end
+    sanitized_rows
+  end
+end
