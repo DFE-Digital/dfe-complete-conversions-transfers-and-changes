586fb62859250d56bdc6bcad9448ea58dc8e10ef
meyric <meyric@dxw.com>
2023-05-09 14:52:38 +0100

    Update path helpers
    Now that we no longer require a 'involuntary conversions' all these path
    helpers could be removed. Before we do that we are taking the first step
    and only returning the voluntary paths.
    
    When we refactor voluntary projects into 'conversion' projects this will
    likely disappear.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,5 @@
 class ApplicationMailer < Mail::Notify::Mailer
   def url_to_project(project)
-    return conversions_involuntary_project_url(project) if project.task_list.is_a?(Conversion::Involuntary::TaskList)
     return conversions_voluntary_project_url(project) if project.task_list.is_a?(Conversion::Voluntary::TaskList)
   end
 end
