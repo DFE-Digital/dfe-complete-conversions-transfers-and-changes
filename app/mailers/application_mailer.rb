1f0338cb989de2214996ccd6b6e9fe619b5ca220
meyric <meyric@dxw.com>
2023-05-11 09:10:47 +0100

    Fix url helper for mailer
    This probably doesn't need to exist, it certainly doesn't beed to check
    the task list type, since the new routes were introduced, this will
    always be `project_url`.
    
    We'll leave it in for now as we are focused on keeping this work as
    small as we can to introduce the new task list.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < Mail::Notify::Mailer
   def url_to_project(project)
-    return project_url(project) if project.task_list.is_a?(Conversion::Voluntary::TaskList)
+    project_url(project)
   end
 end
