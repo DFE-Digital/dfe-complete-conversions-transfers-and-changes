3941dd30bfd18f5c5099a7b586d754b567c4ebc1
Laura Porter <laura@dxw.com>
2023-05-30 12:45:56 +0100

    Add a `contact_id` field for the `funding_agreement_contact` task
    The voluntary conversion task data needs a `contact_id` field for
    `funding_agreement_contact` (named `funding_agreement_contact_contact_id` as
    per the naming convention).

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_17_131621) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_25_154235) do
   create_table "contacts", id: :uuid, default: -> { "newid()" }, force: :cascade do |t|
     t.uuid "project_id"
     t.string "name", null: false
@@ -144,6 +144,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_17_131621) do
     t.boolean "sponsored_support_grant_not_applicable"
     t.boolean "handover_not_applicable"
     t.string "academy_details_name"
+    t.uuid "funding_agreement_contact_contact_id"
   end
 
   create_table "local_authorities", id: :uuid, default: -> { "newid()" }, force: :cascade do |t|
