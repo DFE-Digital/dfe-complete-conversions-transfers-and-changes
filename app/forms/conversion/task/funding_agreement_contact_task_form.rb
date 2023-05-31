0f1a4fcb81508502c8fd791117ea0bf1c21dee47
Laura Porter <laura@dxw.com>
2023-05-31 11:53:58 +0100

    Add the Funding Agreement Letters task
    This task was started by @mec but I have changed the implementation.
    
    Instead of a new Project::Contact being added to the project as part of the
    task, instead the user is presented with a list of existing contacts on the
    task page and can choose one as the Funding Agreement Letters contact. This
    contact's ID is then added to the TasksData for the project.
    
    If there are no contacts on a project, the user is directed to add some at the
    existing External Contacts page.
    
    At this point the user is able to change the Funding Agreement Letters contact
    if one has already been chosen, but I'm not entirely sure this is required.

--- app/forms/conversion/task/funding_agreement_contact_task_form.rb
+++ app/forms/conversion/task/funding_agreement_contact_task_form.rb
@@ -1,34 +1,14 @@
 class Conversion::Task::FundingAgreementContactTaskForm < ::BaseTaskForm
-  attribute :name, :string
-  attribute :email, :string
-  attribute :title, :string
-
-  validates :name, :email, :title, presence: true
-  validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}
+  attribute :contact_id, :string
 
   def initialize(tasks_data, user)
     @tasks_data = tasks_data
     @user = user
-    @project = tasks_data.project
-    @contact = Contact::Project.find_or_create_by(project: @project, funding_agreement_contact: true)
 
     super(@tasks_data, @user)
-
-    assign_attributes(
-      name: @contact.name,
-      title: @contact.title,
-      email: @contact.email
-    )
   end
 
   def save
-    @contact.assign_attributes(
-      project: @project,
-      name: name,
-      title: title,
-      email: email,
-      funding_agreement_contact: true
-    )
-    @contact.save!
+    @tasks_data.update(funding_agreement_contact_contact_id: contact_id)
   end
 end
