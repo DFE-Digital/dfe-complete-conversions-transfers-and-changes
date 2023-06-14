9a7e0c97d5ddac03cead473706d2224f97ff0d31
meyric <meyric@dxw.com>
2023-06-14 12:35:17 +0100

    Add the region to the unassigned projects view
    Regional casework services need to know which region a project is in
    when making a decision about assignment.
    
    Here we add the column to the table that is render in that view.

---
+++ spec/features/projects/view_unassigend_team_projects_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.feature "Viewing unassigned team projects" do
+  before do
+    sign_in_with_user(user)
+    mock_all_academies_api_responses
+  end
+
+  context "as a regional casework services lead" do
+    let(:user) { create(:user, :team_leader) }
+
+    scenario "they can view all unassigned projects in the team" do
+      matching_project = create(:conversion_project, assigned_to: nil, assigned_to_regional_caseworker_team: true)
+
+      visit unassigned_team_projects_path
+
+      expect(page).to have_content(matching_project.urn)
+      expect(page).to have_content(matching_project.establishment.region_name)
+    end
+  end
+end
