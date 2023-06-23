7de6e8ee0e01a9ef43827d3379dd0b5ae280339c
meyric <meyric@dxw.com>
2023-06-23 13:26:13 +0100

    Service to fetch projects by local authority
    Some users are 'Local authority relationship managers' and want to see
    the projects in a given local authority.
    
    Here we build a service to wrap the steps invloved, with the aim of
    returning a list of local authorities that have projects in the
    applicaiton.
    
    First we fetch all projects that are not complete and group them by
    their `establishment.local_authority_code`.
    
    We then fetch the local authorities useing the codes, loop over them
    counting the projects as we go.

---
+++ app/services/by_local_authority_project_fetcher_service.rb
@@ -0,0 +1,31 @@
+class ByLocalAuthorityProjectFetcherService
+  def call
+    conversion_counts = conversions_count_by_local_authority
+
+    if conversion_counts
+      local_authorities = LocalAuthority.where(code: conversion_counts.keys)
+
+      sort_local_authorities_by_name(build_local_authorities_objects(local_authorities, conversion_counts))
+    end
+  end
+
+  private def conversions_count_by_local_authority
+    Project.not_completed.select(:id, :urn).group_by { |p| p.establishment.local_authority_code }
+  end
+
+  private def build_local_authorities_objects(local_authorities, conversion_counts)
+    return [] unless local_authorities.any? && conversion_counts.any?
+
+    local_authorities.map do |local_authority|
+      OpenStruct.new(
+        name: local_authority.name,
+        code: local_authority.code,
+        conversion_count: conversion_counts.fetch(local_authority.code).count
+      )
+    end
+  end
+
+  private def sort_local_authorities_by_name(local_authority_objects)
+    local_authority_objects.sort_by { |local_authority_object| local_authority_object.name }
+  end
+end
---
+++ spec/services/by_local_authority_project_fetcher_service_spec.rb
@@ -0,0 +1,46 @@
+require "rails_helper"
+
+RSpec.describe ByLocalAuthorityProjectFetcherService do
+  it "returns a sorted list of simple local authority objects with projects" do
+    establishment = build(:academies_api_establishment, local_authority_code: "909", urn: 121813)
+    another_establishment = build(:academies_api_establishment, local_authority_code: "213", urn: 121102)
+    yet_another_establishment = build(:academies_api_establishment, local_authority_code: "926", urn: 121583)
+
+    fake_client = double(Api::AcademiesApi::Client,
+      get_trust: Api::AcademiesApi::Client::Result.new(double, nil))
+
+    allow(Api::AcademiesApi::Client).to receive(:new).and_return(fake_client)
+    allow(fake_client).to receive(:get_establishment).with(establishment.urn).and_return(Api::AcademiesApi::Client::Result.new(establishment, nil))
+    allow(fake_client).to receive(:get_establishment).with(another_establishment.urn).and_return(Api::AcademiesApi::Client::Result.new(another_establishment, nil))
+    allow(fake_client).to receive(:get_establishment).with(yet_another_establishment.urn).and_return(Api::AcademiesApi::Client::Result.new(yet_another_establishment, nil))
+
+    create(:local_authority, code: "909", name: "Cumbria County Council")
+    create(:local_authority, code: "213", name: "Westminster City Council")
+    create(:local_authority, code: "926", name: "Norfolk County Council")
+
+    create(:conversion_project, urn: establishment.urn)
+    create(:conversion_project, urn: another_establishment.urn)
+    create(:conversion_project, urn: establishment.urn)
+    create(:conversion_project, urn: yet_another_establishment.urn)
+
+    result = described_class.new.call
+
+    expect(result.count).to eql 3
+
+    first_result = result.first
+
+    expect(first_result.name).to eql "Cumbria County Council"
+    expect(first_result.code).to eql "909"
+    expect(first_result.conversion_count).to eql 2
+
+    last_result = result.last
+
+    expect(last_result.name).to eql "Westminster City Council"
+    expect(last_result.code).to eql "213"
+    expect(last_result.conversion_count).to eql 1
+  end
+
+  it "returns an empty array when there are no projects to source trusts" do
+    expect(described_class.new.call).to eql []
+  end
+end
