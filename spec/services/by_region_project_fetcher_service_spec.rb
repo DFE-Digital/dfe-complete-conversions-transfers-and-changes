8ffb7ceb83447c1467c972d5a549d38484800893
meyric <meyric@dxw.com>
2023-06-26 16:15:39 +0100

    Add projects by region service
    Along with the others (by local authority and by trust) this is another
    useful view of all projects.
    
    As we copy the value for region into the Project, we can do eveything in
    the database.

---
+++ spec/services/by_region_project_fetcher_service_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+RSpec.describe ByRegionProjectFetcherService do
+  it "returns a sorted list of simple view objects with project counts" do
+    mock_successful_api_response_to_create_any_project
+
+    create(:conversion_project, region: "south_west")
+    create(:conversion_project, region: "london")
+    create(:conversion_project, region: "south_west")
+    create(:conversion_project, region: "north_west")
+
+    result = described_class.new.call
+
+    expect(result.count).to eql 3
+
+    first_result = result.first
+
+    expect(first_result.name).to eql "london"
+    expect(first_result.conversion_count).to eql 1
+
+    last_result = result.last
+
+    expect(last_result.name).to eql "south_west"
+    expect(last_result.conversion_count).to eql 2
+  end
+
+  it "returns an empty array when there are no projects to source trusts" do
+    expect(described_class.new.call).to eql []
+  end
+end
