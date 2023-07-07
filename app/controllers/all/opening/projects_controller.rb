7117cb590715ce64032db699323366a5480da7da
meyric <meyric@dxw.com>
2023-07-07 12:38:22 +0100

    Move the opening csv exporter
    We are still working away on this export, and it is now no longer
    completely related to the opening list so we don't want to trigger the
    download there.
    
    For now, we'll add this idea of 'exports' which will let us have
    different kinds of exports for different needs.

--- app/controllers/all/opening/projects_controller.rb
+++ app/controllers/all/opening/projects_controller.rb
@@ -25,19 +25,6 @@ class All::Opening::ProjectsController < ApplicationController
     redirect_to action: "revised", month: month, year: year
   end
 
-  def download_csv
-    authorize Project, :index?
-
-    @projects = ConversionProjectsFetcherService.new.sorted_openers(month, year)
-    @date = "#{Date::MONTHNAMES[month.to_i]} #{year}"
-    @month = month
-    @year = year
-
-    @csv = OpeningProjectsCsvExporter.new(@projects).call
-
-    send_data @csv, filename: "opening_#{month}_#{year}.csv", type: :csv, disposition: "attachment"
-  end
-
   private def month
     params[:month]
   end
