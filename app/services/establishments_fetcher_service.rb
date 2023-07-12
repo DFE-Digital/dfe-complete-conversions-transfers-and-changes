839040788e3d93b5e16243a170d18ded20fcc0b9
meyric <meyric@dxw.com>
2023-07-12 09:40:11 +0100

    Handle errors in the establishment fetcher
    We saw some instances of this in production, we think this is caused by
    the academies api timing out whilst fetching in bulk.
    
    Here we check for any errors in the response and raise if there is one.
    We have also added a belt and braces log to Application Insights.

--- app/services/establishments_fetcher_service.rb
+++ app/services/establishments_fetcher_service.rb
@@ -5,16 +5,31 @@ class EstablishmentsFetcherService
   end
 
   def call!
-    return unless @projects&.any?
+    return if @projects.nil? || @projects.empty?
     raise ArgumentError.new("You must pass in the result of an ActiveRecord query (ActiveRecord::Relation)") unless @projects.is_a?(ActiveRecord::Relation)
 
     @projects.in_batches(of: 20) do |batch_of_projects|
       urns = batch_of_projects.pluck(:urn)
-      @establishments += Api::AcademiesApi::Client.new.get_establishments(urns).object
+      response = Api::AcademiesApi::Client.new.get_establishments(urns)
+
+      if response.error.nil?
+        @establishments.concat(response.object)
+      else
+        track_error(response.error)
+        raise Api::AcademiesApi::Client::Error.new
+      end
     end
 
     @projects.each do |project|
       project.establishment = @establishments.find { |establishment| establishment.urn == project.urn.to_s }
+    end
+  end
+
+  private def track_error(error_message)
+    if ENV.fetch("APPLICATION_INSIGHTS_KEY", nil)
+      tc = ApplicationInsights::TelemetryClient.new(ENV.fetch("APPLICATION_INSIGHTS_KEY"))
+      tc.track_event(error_message)
+      tc.flush
     end
   end
 end
