7947cd66e616086dba91846e9d47154f281ecad4
meyric <meyric@dxw.com>
2023-07-20 10:45:04 +0100

    Add a new Academies API pre fetcher service
    We've got in an bit of a mess with all the prefetching we have to do to
    speed up the applicaiton interface.
    
    In summary, fetching multiple entities from the Academies API is far
    quicker than having to do a single request-response for every project.
    We 'inject' the fetched establishments and trusts back into the projects
    and then use them in views.
    
    We have quiet a few different ways to do this, and multiple failure
    states. This works aims to become the only way to pre fetch Academies
    API data.
    
    I does so in batches of 10 and can handle eager loading of associations,
    we have had issues with both of these in the past.
    
    Unfortunately, there is no fast way to create enough projects to really
    test this other than creating > 10 projects in the database, for me, it
    is worth the speed penalty.
    
    Once again, I am mocking the API client in the spec to keep it close to
    hand and document what is actually happening - it maybe we can come up
    with a way to do this better, for now I would like to keep this.
    
    We won't get rid of the old idividual establishment_fetcher_service and
    trust_fetcher_service as those are still in use. Once we are confident
    this approach is working well, we plan to swap over.

---
+++ spec/services/academies_api_pre_fetcher_service_spec.rb
@@ -0,0 +1,97 @@
+require "rails_helper"
+
+RSpec.describe AcademiesApiPreFetcherService do
+  it "prefetches Academies API data in batches of 10 and populates projects" do
+    api_client = mock_academies_api_client_get_establishments_and_trusts
+
+    25.times do
+      create(:conversion_project, urn: 123456, incoming_trust_ukprn: 10010010)
+    end
+
+    projects = Project.all
+
+    AcademiesApiPreFetcherService.new.call!(projects)
+
+    expect(projects.last.establishment).not_to be_nil
+    expect(projects.last.incoming_trust).not_to be_nil
+
+    expect(api_client).to have_received(:get_establishments).exactly(3).times
+    expect(api_client).to have_received(:get_trusts).exactly(3).times
+
+    expect(api_client).to have_received(:get_establishment).exactly(25).times
+    expect(api_client).to have_received(:get_trust).exactly(25).times
+  end
+
+  it "handles eager loading" do
+    mock_academies_api_client_get_establishments_and_trusts
+
+    create(:conversion_project, urn: 123456, incoming_trust_ukprn: 10010010)
+
+    projects = Project.all.includes(:tasks_data)
+
+    expect(projects.last.establishment).not_to be_nil
+    expect(projects.last.incoming_trust).not_to be_nil
+
+    expect(projects.last.all_conditions_met?).to be false
+  end
+
+  it "raises and Academies API error if the establishments requests fail" do
+    mock_academies_api_client_get_establishments_and_trusts_failure
+
+    10.times do
+      create(:conversion_project, urn: 123456, incoming_trust_ukprn: 10010010)
+    end
+
+    projects = Project.all
+
+    expect { AcademiesApiPreFetcherService.new.call!(projects) }.to raise_error(Api::AcademiesApi::Client::Error)
+  end
+
+  it "raises and Academies API error if the trusts requests fail" do
+    establishment = double("Establishment", name: "Establishment Name", urn: "123456")
+    api_client = mock_academies_api_client_get_establishments_and_trusts_failure
+    allow(api_client).to receive(:get_establishments).and_return(Api::AcademiesApi::Client::Result.new([establishment], nil))
+
+    10.times do
+      create(:conversion_project, urn: 123456, incoming_trust_ukprn: 10010010)
+    end
+
+    projects = Project.all
+
+    expect { AcademiesApiPreFetcherService.new.call!(projects) }.to raise_error(Api::AcademiesApi::Client::Error)
+  end
+
+  def mock_academies_api_client_get_establishments_and_trusts
+    api_client = Api::AcademiesApi::Client.new
+
+    establishment = double("Establishment", name: "Establishment Name", urn: "123456")
+    trust = double("Trust", ukprn: "10010010")
+
+    allow(Api::AcademiesApi::Client).to receive(:new).and_return(api_client)
+
+    allow(api_client).to receive(:get_establishments).and_return(Api::AcademiesApi::Client::Result.new([establishment], nil))
+    allow(api_client).to receive(:get_trusts).and_return(Api::AcademiesApi::Client::Result.new([trust], nil))
+
+    allow(api_client).to receive(:get_trust).and_return(Api::AcademiesApi::Client::Result.new(establishment, nil))
+    allow(api_client).to receive(:get_establishment).and_return(Api::AcademiesApi::Client::Result.new(trust, nil))
+
+    api_client
+  end
+
+  def mock_academies_api_client_get_establishments_and_trusts_failure
+    api_client = Api::AcademiesApi::Client.new
+
+    establishment = double("Establishment", name: "Establishment Name", urn: "123456")
+    trust = double("Trust", ukprn: "10010010")
+
+    allow(Api::AcademiesApi::Client).to receive(:new).and_return(api_client)
+
+    allow(api_client).to receive(:get_establishments).and_return(Api::AcademiesApi::Client::Result.new(nil, Api::AcademiesApi::Client::Error.new))
+    allow(api_client).to receive(:get_trusts).and_return(Api::AcademiesApi::Client::Result.new(nil, Api::AcademiesApi::Client::Error.new))
+
+    allow(api_client).to receive(:get_trust).and_return(Api::AcademiesApi::Client::Result.new(establishment, nil))
+    allow(api_client).to receive(:get_establishment).and_return(Api::AcademiesApi::Client::Result.new(trust, nil))
+
+    api_client
+  end
+end
