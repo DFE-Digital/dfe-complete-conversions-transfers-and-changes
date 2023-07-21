70f4aa4016e503b54ed03dc429e353d225425613
meyric <meyric@dxw.com>
2023-07-21 17:48:38 +0100

    Update exports controller
    We want this to use the new Academies API pre fetching.

--- app/services/projects_for_export_service.rb
+++ app/services/projects_for_export_service.rb
@@ -1,22 +1,15 @@
 class ProjectsForExportService
   def risk_protection_arrangement_projects(month:, year:)
     projects = projects_by_month_and_year(month, year)
-    pre_fetch_entities!(projects)
-    projects
+    AcademiesApiPreFetcherService.new.call!(projects)
   end
 
   def funding_agreement_letters_projects(month:, year:)
     projects = projects_by_month_and_year(month, year)
-    pre_fetch_entities!(projects)
-    projects
+    AcademiesApiPreFetcherService.new.call!(projects)
   end
 
   private def projects_by_month_and_year(month, year)
     Conversion::Project.opening_by_month_year(month, year)
-  end
-
-  private def pre_fetch_entities!(projects)
-    EstablishmentsFetcherService.new(projects).batched!
-    TrustsFetcherService.new(projects).batched!
   end
 end
