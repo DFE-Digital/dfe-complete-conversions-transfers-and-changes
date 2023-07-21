db4742db9282968434a735d9622df82c2c069ec0
Laura Porter <laura@dxw.com>
2023-07-21 11:10:39 +0100

    Show "2 Requires improvement" status on Project information page

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
 - Add "2RI" question to the new Conversion project form
 - Remove hint from the "DAO" question on the create Conversion project form
+- Show the "2RI" value on the Project information page
 
 ### Changed
 
--- spec/features/project_information/users_can_view_project_details_spec.rb
+++ spec/features/project_information/users_can_view_project_details_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.feature "Users can view project details" do
   let(:user) { create(:user, :caseworker) }
-  let(:project) { create(:conversion_project, directive_academy_order: true) }
+  let(:project) { create(:conversion_project, directive_academy_order: true, two_requires_improvement: true) }
 
   before do
     mock_successful_api_responses(urn: any_args, ukprn: any_args)
@@ -11,7 +11,13 @@ RSpec.feature "Users can view project details" do
   end
 
   scenario "they can see if it has had a directive academy order issued" do
-    within("#projectDetails .govuk-summary-list__row:first-of-type") do
+    within("#projectDetails .govuk-summary-list__row:contains('Has a directive academy order been issued?')") do
+      expect(page).to have_content("Yes")
+    end
+  end
+
+  scenario "they can see if it has had two 'Requires improvement' ratings" do
+    within("#projectDetails .govuk-summary-list__row:contains('Is this conversion due to intervention following 2RI?')") do
       expect(page).to have_content("Yes")
     end
   end
