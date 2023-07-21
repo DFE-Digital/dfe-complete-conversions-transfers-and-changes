7c28f7971c67509855e0ed3aa9945cbffe21cbfc
meyric <meyric@dxw.com>
2023-07-21 14:55:23 +0100

    Update the Academies API pre fetcher
    We don't actually want to update the projects with the fetched entities
    until we have them all.
    
    It's only the API request that we want to batch up.
    
    Doing this also seems to have cured a lot of Bullet issues we were
    seeeing and means we don't have to skip Bullet.

--- app/services/academies_api_pre_fetcher_service.rb
+++ app/services/academies_api_pre_fetcher_service.rb
@@ -9,31 +9,21 @@ class AcademiesApiPreFetcherService
     establishments = []
     incoming_trusts = []
 
-    # Bullet has to be disabled here to prevent false positives, this code
-    # fetches data from the Academies APi and sets the associations on the
-    # projects but does not use any eager loaded associations that were passed
-    # in and this triggers bullet
-    #
-    # If the calling code then didn't use the eager loading, it would be
-    # captured there.
-    #
-    # We re-enable Bullet once this code has executed
-    Bullet.enable = false if defined?(Bullet)
     @projects.find_in_batches(batch_size: ENV.fetch("ACADEMIES_API_BATCH_SIZE", 20).to_i) do |projects|
       establishment_urns = projects.pluck(:urn).compact
       incoming_trusts_ukprns = projects.pluck(:incoming_trust_ukprn).compact
 
       if establishment_urns.any?
         establishments.concat(pre_fetch_establishments(establishment_urns))
-        set_establishments!(establishments)
       end
 
       if incoming_trusts_ukprns.any?
         incoming_trusts.concat(pre_fetch_trusts(incoming_trusts_ukprns))
-        set_incoming_trusts!(incoming_trusts)
       end
     end
-    Bullet.enable = true if defined?(Bullet)
+
+    set_establishments!(establishments) if establishments.any?
+    set_incoming_trusts!(incoming_trusts) if incoming_trusts.any?
   end
 
   private def set_establishments!(establishments)
