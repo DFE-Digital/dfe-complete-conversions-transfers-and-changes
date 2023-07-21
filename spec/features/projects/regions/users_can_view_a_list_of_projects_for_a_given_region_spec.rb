44fbed056ecc6baf57c71e5a24330940ac476fc6
meyric <meyric@dxw.com>
2023-07-21 13:52:03 +0100

    Update All projects > By region controller
    To use the new Academies API pre fetching.

--- spec/features/projects/regions/users_can_view_a_list_of_projects_for_a_given_region_spec.rb
+++ spec/features/projects/regions/users_can_view_a_list_of_projects_for_a_given_region_spec.rb
@@ -18,6 +18,16 @@ RSpec.feature "Users can view a list of projects for a given region" do
       expect(page).to have_content(project.urn)
       expect(page).not_to have_content(completed_project.urn)
     end
+
+    scenario "when there are enough projects to page they see a pager" do
+      21.times do
+        create(:conversion_project, region: :london)
+      end
+
+      visit by_region_all_regions_projects_path(:london)
+
+      expect(page).to have_css(".govuk-pagination")
+    end
   end
 
   context "when a project is unassigned" do
