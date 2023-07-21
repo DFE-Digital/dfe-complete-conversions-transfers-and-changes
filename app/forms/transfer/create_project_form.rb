1c75b5ab5b145eece433015e5105857100a204e3
Laura Porter <laura@dxw.com>
2023-07-21 16:03:31 +0100

    Add a Create form for Transfer projects
    This form validates the required attributes for both Projects in general, and
    the required attributes for Transfer::Projects

---
+++ app/forms/transfer/create_project_form.rb
@@ -0,0 +1,115 @@
+class Transfer::CreateProjectForm
+  include ActiveModel::Model
+  include ActiveModel::Attributes
+  include ActiveRecord::AttributeAssignment
+
+  SHAREPOINT_URLS = %w[educationgovuk-my.sharepoint.com educationgovuk.sharepoint.com].freeze
+
+  class NegativeValueError < StandardError; end
+
+  attribute :urn, :integer
+  attribute :incoming_trust_ukprn, :integer
+  attribute :outgoing_trust_ukprn, :integer
+  attribute :establishment_sharepoint_link
+  attribute :trust_sharepoint_link
+  attribute :user
+
+  attr_reader :advisory_board_date
+
+  validates :urn, presence: true, urn: true
+  validates :incoming_trust_ukprn, presence: true, ukprn: true
+  validates :outgoing_trust_ukprn, presence: true, ukprn: true
+  validates :advisory_board_date, presence: true
+  validates :advisory_board_date, date_in_the_past: true
+
+  validates :establishment_sharepoint_link, presence: true, url: {hostnames: SHAREPOINT_URLS}
+  validates :trust_sharepoint_link, presence: true, url: {hostnames: SHAREPOINT_URLS}
+
+  validate :establishment_exists, if: -> { urn.present? }
+  validate :trust_exists, if: -> { incoming_trust_ukprn.present? }
+
+  validate :urn_unique_for_in_progress_transfers, if: -> { urn.present? }
+
+  validate :multiparameter_date_attributes_values
+
+  def initialize(params = {})
+    @attributes_with_invalid_values = []
+    super(params)
+  end
+
+  def save
+    @project = Transfer::Project.new(
+      urn: urn,
+      incoming_trust_ukprn: incoming_trust_ukprn,
+      outgoing_trust_ukprn: incoming_trust_ukprn,
+      establishment_sharepoint_link: establishment_sharepoint_link,
+      trust_sharepoint_link: trust_sharepoint_link,
+      advisory_board_date: advisory_board_date,
+      regional_delivery_officer_id: user.id,
+      team: user.team,
+      assigned_to: user,
+      assigned_at: DateTime.now,
+      region: region,
+      tasks_data: Transfer::TasksData.new
+    )
+
+    return nil unless valid?
+
+    ActiveRecord::Base.transaction do
+      @project.save
+    end
+
+    @project
+  end
+
+  def region
+    @region = establishment.region_code
+  end
+
+  def advisory_board_date=(hash)
+    @advisory_board_date = Date.new(value_at_position(hash, 1), value_at_position(hash, 2), value_at_position(hash, 3))
+  rescue NoMethodError
+    nil
+  rescue TypeError, Date::Error, NegativeValueError
+    @attributes_with_invalid_values << :advisory_board_date
+  end
+
+  private def establishment
+    @establishment || fetch_establishment(urn)
+  end
+
+  private def establishment_exists
+    establishment
+  rescue Api::AcademiesApi::Client::NotFoundError
+    errors.add(:urn, :no_establishment_found)
+  end
+
+  private def trust_exists
+    result = Api::AcademiesApi::Client.new.get_trust(incoming_trust_ukprn)
+    raise result.error if result.error.present?
+  rescue Api::AcademiesApi::Client::NotFoundError
+    errors.add(:incoming_trust_ukprn, :no_trust_found)
+  end
+
+  private def fetch_establishment(urn)
+    result = Api::AcademiesApi::Client.new.get_establishment(urn)
+    raise result.error if result.error.present?
+
+    result.object
+  end
+
+  private def urn_unique_for_in_progress_transfers
+    errors.add(:urn, :duplicate) if Transfer::Project.not_completed.where(urn: urn).any?
+  end
+
+  private def value_at_position(hash, position)
+    value = hash[position]
+    return NegativeValueError if value.to_i < 0
+    value
+  end
+
+  private def multiparameter_date_attributes_values
+    return if @attributes_with_invalid_values.empty?
+    @attributes_with_invalid_values.each { |attribute| errors.add(attribute, :invalid) }
+  end
+end
---
+++ spec/factories/transfer/create_project_form_factory.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :create_transfer_project_form, class: "Transfer::CreateProjectForm" do
+    urn { 123456 }
+    incoming_trust_ukprn { 10061021 }
+    outgoing_trust_ukprn { 10066123 }
+    advisory_board_date { {3 => 1, 2 => 10, 1 => 2022} }
+    establishment_sharepoint_link { "https://educationgovuk-my.sharepoint.com/establishment-folder" }
+    trust_sharepoint_link { "https://educationgovuk-my.sharepoint.com/trust-folder" }
+    user { association :user, :regional_delivery_officer }
+  end
+end
