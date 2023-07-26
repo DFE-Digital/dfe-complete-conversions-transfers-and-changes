5a8027e1b47e42af9351d168471ea74e0785de26
meyric <meyric@dxw.com>
2023-07-26 21:53:22 +0100

    Support project information for transfers
    We have the approach for all these project sub sections now. Here we
    allow a transfer to render it's project information.
    
    This involves moving the existing views to the conversions namespace,
    createing new transfers view and sharing as much as possible via
    partials.

--- spec/features/transfers/users_can_view_a_transfer_spec.rb
+++ spec/features/transfers/users_can_view_a_transfer_spec.rb
@@ -9,10 +9,30 @@ RSpec.feature "Users can view a transfer" do
 
   let(:transfer_project) { create(:transfer_project) }
 
-  scenario "via the tasks route" do
+  scenario "they can view the summary" do
     visit project_path(transfer_project)
 
     expect(page).to have_content(transfer_project.urn)
-    expect(page).to have_content("Transfer")
+    expect(page).to have_content("transfer")
+    expect(page).to have_content("West Midlands")
+  end
+
+  scenario "they can view the task list" do
+    visit project_path(transfer_project)
+
+    expect(page).to have_content(transfer_project.urn)
+    expect(page).to have_content("Task list")
+  end
+
+  scenario "they can view the project information" do
+    visit project_information_path(transfer_project)
+
+    expect(page).to have_content(transfer_project.urn)
+    expect(page).to have_content("Project details")
+    expect(page).to have_content("Advisory board details")
+    expect(page).to have_content("Academy details")
+    expect(page).to have_content("Incoming trust details")
+    expect(page).to have_content("Outgoing trust details")
+    expect(page).to have_content("Diocese details")
   end
 end
