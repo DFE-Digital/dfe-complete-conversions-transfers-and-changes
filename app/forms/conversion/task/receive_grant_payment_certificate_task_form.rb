fb72fb5c645566240ae856f7eb42a02c2e7ce3cd
Laura Porter <laura@dxw.com>
2023-08-01 10:20:22 +0100

    Amendments to "Receive grant payment certificate" task
    Remove the third checkbox (`update_sheet`) from this task, and make the guidance
    on the first checkbox more explicit.

--- app/forms/conversion/task/receive_grant_payment_certificate_task_form.rb
+++ app/forms/conversion/task/receive_grant_payment_certificate_task_form.rb
@@ -1,5 +1,4 @@
 class Conversion::Task::ReceiveGrantPaymentCertificateTaskForm < BaseTaskForm
   attribute :check_and_save, :boolean
   attribute :update_kim, :boolean
-  attribute :update_sheet, :boolean
 end
