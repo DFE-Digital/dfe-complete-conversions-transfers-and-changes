7dfd62dc41f2219f73f18a6e8bdbb12aa8f27f69
Laura Porter <laura@dxw.com>
2023-08-02 11:16:02 +0100

    All `ByMonthPrrojectFetcherService` methods return Conversions and Transfers
    This commit required some finessing to the specs, mostly in terms of changing
    the mocks to use the generic `significant_date` instead of `conversion_date`

--- spec/requests/all/opening/projects_controller_spec.rb
+++ spec/requests/all/opening/projects_controller_spec.rb
@@ -26,63 +26,68 @@ RSpec.describe All::Opening::ProjectsController, type: :request do
         expect(request.params.fetch(:year)).to eq Date.today.next_month.year.to_s
       end
     end
+  end
 
-    describe "#confirmed" do
-      context "when supplying a month and year" do
-        context "when the year is missing" do
-          it "returns a 404" do
-            get "/projects/all/opening/confirmed/1"
-            expect(response).to have_http_status(:not_found)
-          end
+  describe "#confirmed" do
+    context "when supplying a month and year" do
+      context "when the year is missing" do
+        it "returns a 404" do
+          get "/projects/all/opening/confirmed/1"
+          expect(response).to have_http_status(:not_found)
+        end
+      end
+
+      context "when the month isn't in the scope 1..12" do
+        it "returns a 404" do
+          get "/projects/all/opening/confirmed/13/2022"
+          expect(response).to have_http_status(:not_found)
+        end
+      end
+
+      context "when the year isn't in the scope 2000-2499" do
+        it "returns a 404" do
+          get "/projects/all/opening/confirmed/12/2555"
+          expect(response).to have_http_status(:not_found)
+        end
+      end
+
+      context "when the month and year are present and in scope" do
+        it "shows a page title with the month & year" do
+          get "/projects/all/opening/confirmed/1/2022"
+
+          expect(response.body).to include("Academies opening in January 2022")
         end
 
-        context "when the month isn't in the scope 1..12" do
-          it "returns a 404" do
-            get "/projects/all/opening/confirmed/13/2022"
-            expect(response).to have_http_status(:not_found)
-          end
+        it "returns project details in table form" do
+          conversion_project = create(:conversion_project, conversion_date: Date.new(2022, 1, 1), conversion_date_provisional: false, urn: 100001)
+          transfer_project = create(:transfer_project, transfer_date: Date.new(2022, 1, 1), transfer_date_provisional: false, urn: 100002)
+          get "/projects/all/opening/confirmed/1/2022"
+
+          expect(response.body).to include(
+            conversion_project.establishment.name,
+            conversion_project.urn.to_s,
+            transfer_project.establishment.name,
+            transfer_project.urn.to_s
+          )
         end
 
-        context "when the year isn't in the scope 2000-2499" do
-          it "returns a 404" do
-            get "/projects/all/opening/confirmed/12/2555"
-            expect(response).to have_http_status(:not_found)
-          end
+        it "only returns projects whose confirmed significant date is in that month & year" do
+          conversion_project_in_scope = create(:conversion_project, urn: 100001, conversion_date: Date.new(2022, 1, 1), conversion_date_provisional: false)
+          transfer_project_in_scope = create(:conversion_project, urn: 100003, conversion_date: Date.new(2022, 1, 1), conversion_date_provisional: false)
+          project_not_in_scope = create(:conversion_project, urn: 100002, conversion_date: Date.new(2022, 2, 1), conversion_date_provisional: false)
+
+          get "/projects/all/opening/confirmed/1/2022"
+
+          expect(response.body).to include(conversion_project_in_scope.urn.to_s)
+          expect(response.body).to include(transfer_project_in_scope.urn.to_s)
+          expect(response.body).to_not include(project_not_in_scope.urn.to_s)
         end
 
-        context "when the month and year are present and in scope" do
-          it "shows a page title with the month & year" do
+        context "when there are no academies opening in that month & year" do
+          it "shows a helpful message" do
             get "/projects/all/opening/confirmed/1/2022"
 
-            expect(response.body).to include("Academies opening in January 2022")
-          end
-
-          it "returns project details in table form" do
-            conversion_project = create(:conversion_project, conversion_date: Date.new(2022, 1, 1), conversion_date_provisional: false)
-            get "/projects/all/opening/confirmed/1/2022"
-
-            expect(response.body).to include(
-              conversion_project.establishment.name,
-              conversion_project.urn.to_s
-            )
-          end
-
-          it "only returns projects whose confirmed conversion date is in that month & year" do
-            project_in_scope = create(:conversion_project, urn: 100001, conversion_date: Date.new(2022, 1, 1), conversion_date_provisional: false)
-            project_not_in_scope = create(:conversion_project, urn: 100002, conversion_date: Date.new(2022, 2, 1), conversion_date_provisional: false)
-
-            get "/projects/all/opening/confirmed/1/2022"
-
-            expect(response.body).to include(project_in_scope.urn.to_s)
-            expect(response.body).to_not include(project_not_in_scope.urn.to_s)
-          end
-
-          context "when there are no academies opening in that month & year" do
-            it "shows a helpful message" do
-              get "/projects/all/opening/confirmed/1/2022"
-
-              expect(response.body).to include("There are currently no schools expected to become academies in January 2022")
-            end
+            expect(response.body).to include("There are currently no schools expected to become academies in January 2022")
           end
         end
       end
