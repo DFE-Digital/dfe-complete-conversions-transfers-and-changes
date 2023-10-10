494128ee10dc65ceb7b0186f06f741b4bc96eada
Laura Porter <laura@dxw.com>
2023-10-10 10:33:23 +0100

    Lightly format the ingestion result for GiasEstablishmentImportMailer
    Add some very basic formatting for the message personalisation in
    GiasEstablishmentImportMailer.

--- app/mailers/gias_establishment_import_mailer.rb
+++ app/mailers/gias_establishment_import_mailer.rb
@@ -4,8 +4,18 @@ class GiasEstablishmentImportMailer < ApplicationMailer
       "316ef413-5e53-48e4-8a78-2aeaa9b98114",
       to: user.email,
       personalisation: {
-        result: result
+        result: format_result(result)
       }
     )
+  end
+
+  private def format_result(result)
+    <<~TEXT
+      Total CSV rows: #{result[:result][:total_csv_rows]}
+      New records: #{result[:result][:new_records]}
+      Changed records: #{result[:result][:changed_records]}
+      Time: #{result[:result][:time].strftime("%Y-%m-%d %H:%M")}
+      Errors: #{result[:result][:errors].count}
+    TEXT
   end
 end
