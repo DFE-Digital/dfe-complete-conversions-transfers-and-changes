978b10c9356deb0d83c9a90b3ad35b67d6ca7211
Laura Porter <laura@dxw.com>
2023-10-12 12:04:43 +0100

    Add Contact::Establishment model
    This model is for Contacts who belong to an establishment. They have a
    mandatory `establishment_urn` field.
    
    This has not been created using standard Rails associations (belongs_to/has_one)
    to make it easier to fetch the contacts associated with a Project. A project has
    an establishment via a URN, butthis establishment is *not* a Gias::Establishment
    model, it is instead fetched from an API.
    
    So if we were to associate the new
    Contact::Establishment with a Gias::Establishment via a foreign_key association,
    when it came to fetching the contacts for a project, we would not be able to use
    `project.establishment.contact` because the `establishment` associated with the
    project is not actually a Gias::Establishment.
    
    This works for now but we can add "true" rails associations later if the
    establishment association with projects becomes a Gias::Establishment.

---
+++ spec/models/contact/establishment_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+RSpec.describe Contact::Establishment do
+  describe "Validations" do
+    it { should validate_presence_of(:establishment_urn) }
+  end
+
+  describe "#establishment" do
+    it "returns the establishment by establishment_urn" do
+      contact = described_class.new(establishment_urn: 123456)
+      establishment = create(:gias_establishment, urn: 123456)
+
+      expect(contact.establishment).to eq(establishment)
+    end
+  end
+
+  describe ".policy_class" do
+    it "uses the ContactPolicy" do
+      expect(described_class.policy_class).to eq(ContactPolicy)
+    end
+  end
+
+  describe "#editable" do
+    it "always returns false" do
+      contact = described_class.new(establishment_urn: 123456)
+
+      expect(contact.editable?).to be false
+    end
+  end
+end
