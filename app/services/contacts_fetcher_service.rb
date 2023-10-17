91b3ea3255464353a3fa7154bcb4bc8b96d5e17d
Laura Porter <laura@dxw.com>
2023-10-17 16:44:22 +0100

    Fetch Contact::Establishment contacts in the ContactsFetcherService

--- app/services/contacts_fetcher_service.rb
+++ app/services/contacts_fetcher_service.rb
@@ -6,6 +6,9 @@ class ContactsFetcherService
     director_of_child_services = project.director_of_child_services
     all_contacts << director_of_child_services unless director_of_child_services.nil?
 
+    establishment_contacts = Contact::Establishment.find_by(establishment_urn: project.urn)
+    all_contacts << establishment_contacts unless establishment_contacts.nil?
+
     return {} unless all_contacts.any?
 
     all_contacts.sort_by(&:name).group_by(&:category)
