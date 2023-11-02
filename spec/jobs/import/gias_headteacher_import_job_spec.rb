95a076023be59ce3cba1f854b60ae60de4fbe48a
meyric <meyric@dxw.com>
2023-11-02 10:55:10 +0000

    Acutally send the GIAS headteacher import email
    When working on the GIAS headteacher importing, we missed actually
    sending the email at the end of the process.

--- spec/jobs/import/gias_headteacher_import_job_spec.rb
+++ spec/jobs/import/gias_headteacher_import_job_spec.rb
@@ -23,6 +23,15 @@ RSpec.describe Import::GiasHeadteacherImportJob, type: :job do
       expect(importer).to have_received(:import!)
     end
 
+    it "queues an email to the user with the import report" do
+      mock_mailer = double(GiasHeadteacherImportMailer, deliver_later: true)
+      expect(GiasHeadteacherImportMailer).to receive(:import_notification).and_return(mock_mailer)
+
+      subject.perform_now(file_path, user)
+
+      expect(mock_mailer).to have_received(:deliver_later).exactly(1).time
+    end
+
     it "deletes the file afterwards" do
       subject.perform_now(file_path, user)
       expect(File).to have_received(:delete).with(file_path)
