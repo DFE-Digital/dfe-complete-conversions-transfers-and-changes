637a1056f34d0022c57b05d85932ffd47dbad8f2
Laura Porter <laura@dxw.com>
2023-11-03 10:29:39 +0000

    Search by words or word-like strings
    We want to be able to search for a project by establishment name. To do this,
    we first have to identify that the query string is a "word like" string and
    not a URN or UKPRN.
    
    Once that is done, we search the GIAS::Establishment records using the query
    against the Establisment names. If we find any matches, we use the URNs of the
    matching establishments and pass them to `search_by_urns`.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
 ## [Unreleased][unreleased]
 
+### Added
+
+- Allow search by words (establishment name)
+
 ## [Release-44][release-44]
 
 ### Fixed
--- app/services/project_search_service.rb
+++ app/services/project_search_service.rb
@@ -8,6 +8,10 @@ class ProjectSearchService
       return search_by_ukprn(query)
     end
 
+    if word_pattern(query)
+      return search_by_words(query)
+    end
+
     []
   end
 
@@ -20,11 +24,27 @@ class ProjectSearchService
       .or(Project.where("outgoing_trust_ukprn = ?", ukprn))
   end
 
+  def search_by_words(query)
+    establishment_results = search_establishments_by_name(query)
+    return [] if establishment_results.empty?
+
+    urns = establishment_results.pluck(:urn)
+    search_by_urns(urns)
+  end
+
+  private def search_establishments_by_name(query)
+    Gias::Establishment.where("LOWER(name) LIKE ?", "%#{query.downcase}%")
+  end
+
   private def urn_pattern(query)
     query.match?(/^\d{6}$/)
   end
 
   private def ukprn_pattern(query)
     query.match?(/^\d{8}$/)
+  end
+
+  private def word_pattern(query)
+    query.match?(/\D/)
   end
 end
