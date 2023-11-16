c2f7acb8f2da4962565f3e29b838a8718b905914
Laura Porter <laura@dxw.com>
2023-11-16 17:14:39 +0000

    Add a basic view for the Grant management and finance unit export page
    This page shows a list of the previous 12 months and allows the user to download
    a list of projects whose Advisory board dates are in those months.
    
    The other export pages show the next 6 month,s but because Advisory board dates
    are in the past, we should show the current month & previous 11 (to make up a
    full year)
    
    This page is currently ont linked from anywhere and is not accessible to any
    particular user group yet.

---
+++ app/controllers/all/export/grant_management_and_finance_unit/conversions/projects_controller.rb
@@ -0,0 +1,30 @@
+class All::Export::GrantManagementAndFinanceUnit::Conversions::ProjectsController < ApplicationController
+  def index
+    authorize Project, :index?
+
+    @months = export_months
+  end
+
+  def csv
+    authorize Project, :index?
+
+    projects = ProjectsForExportService.new.grant_management_and_finance_unit_projects(month: month, year: year)
+    csv = Export::GrantManagementAndFinanceUnitCsvExportService.new(projects).call
+
+    send_data csv, filename: "#{year}-#{month}_grant_management_and_finance_unit_export.csv", type: :csv, disposition: "attachment"
+  end
+
+  private def month
+    params[:month]
+  end
+
+  private def year
+    params[:year]
+  end
+
+  private def export_months
+    12.times.map do |index|
+      Date.today.at_beginning_of_month - index.months
+    end
+  end
+end
