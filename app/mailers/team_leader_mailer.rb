c623c836b387d15cb39222860d947252806923c3
Laura Porter <laura@dxw.com>
2023-12-15 11:54:38 +0000

    (Refactor) Rename notification method for new projects being created
    `TeamLeaderMailer.new_project_created` is notifying Team leaders only when a
    new Conversion project is created, so rename it to make this more clear.

--- app/mailers/team_leader_mailer.rb
+++ app/mailers/team_leader_mailer.rb
@@ -1,5 +1,5 @@
 class TeamLeaderMailer < ApplicationMailer
-  def new_project_created(team_leader, project)
+  def new_conversion_project_created(team_leader, project)
     template_mail(
       "ea4f72e4-f5bb-4b1a-b5f9-a94cc1840353",
       to: team_leader.email,
--- spec/mailers/team_leader_mailer_spec.rb
+++ spec/mailers/team_leader_mailer_spec.rb
@@ -1,13 +1,13 @@
 require "rails_helper"
 
 RSpec.describe TeamLeaderMailer do
-  describe "#new_project_created" do
+  describe "#new_conversion_project_created" do
     let(:team_leader) { create(:user, :team_leader) }
     let(:project) { create(:conversion_project) }
     let(:template_id) { "ea4f72e4-f5bb-4b1a-b5f9-a94cc1840353" }
     let(:expected_personalisation) { {first_name: team_leader.first_name, project_url: project_url(project.id)} }
 
-    subject(:send_mail) { described_class.new_project_created(team_leader, project).deliver_now }
+    subject(:send_mail) { described_class.new_conversion_project_created(team_leader, project).deliver_now }
 
     before { mock_successful_api_responses(urn: 123456, ukprn: 10061021) }
 
