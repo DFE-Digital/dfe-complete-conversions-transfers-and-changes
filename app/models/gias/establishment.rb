fc722e15d13693fe0cd8651d93405c2bcaa181e7
Laura Porter <laura@dxw.com>
2024-01-19 15:04:01 +0000

    Add helper methods to Gias::Establishment
    We are going to start using Gias::Establishment models (from our own db) instead
    of the Api::AcademiesApi::Establishment transient models built from Academy
    API responses. In order to swap these models like-for-like, we need to duplicate
    five methods the Api::AcademiesApi::Establishment model has: phase, address,
    local_authority, has_diocese? and dfe_number.
    
    This ensures we can still use the same front-end views to display both
    model types.

--- app/models/gias/establishment.rb
+++ app/models/gias/establishment.rb
@@ -2,4 +2,31 @@ class Gias::Establishment < ApplicationRecord
   self.table_name = :gias_establishments
 
   validates :urn, presence: true
+
+  alias_attribute :phase, :phase_name
+
+  DIOCESE_NOT_APPLICABLE_CODE = "0000"
+
+  def address
+    [
+      address_street,
+      address_locality,
+      address_additional,
+      address_town,
+      address_county,
+      address_postcode
+    ]
+  end
+
+  def local_authority
+    LocalAuthority.find_by(code: local_authority_code)
+  end
+
+  def dfe_number
+    "#{local_authority_code}/#{establishment_number}"
+  end
+
+  def has_diocese?
+    diocese_code != DIOCESE_NOT_APPLICABLE_CODE
+  end
 end
