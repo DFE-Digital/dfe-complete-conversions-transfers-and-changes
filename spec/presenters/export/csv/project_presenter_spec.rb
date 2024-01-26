21056aa844c2d4223879547fb32b2c76348f0519
Laura Porter <laura@dxw.com>
2024-01-26 16:38:01 +0000

    Change "All conditions met" header in exports for Transfer projects
    The header for the "All conditions met" column in the Transfers export should be
    "Authority to proceed"
    
    The simplest way to do this is to add a `authority_to_proceed` alias to the
    Project Presenter, and use this to return the `all_conditions_met` value
    for the Project. Because the name of the method is `authority_to_proceed`, we
    can add a new I18n string to the file with the correct header.
    
    Therefore when we call the column header `authority_to_proceed` in the export,
    we get the I18n string for `authority_to_proceed` as the header, and the value
    of `all_conditions_met?` in the value.

--- spec/presenters/export/csv/project_presenter_spec.rb
+++ spec/presenters/export/csv/project_presenter_spec.rb
@@ -223,20 +223,28 @@ RSpec.describe Export::Csv::ProjectPresenter do
     expect(presenter.all_conditions_met).to eql "yes"
   end
 
-  it "presents all conditions met when it has not been met (nil)" do
-    project = double(Conversion::Project, all_conditions_met: nil)
+  it "presents authority to proceed, which is the name for 'all conditions met' for Transfer projects" do
+    project = double(Transfer::Project, all_conditions_met: true)
 
     presenter = described_class.new(project)
 
-    expect(presenter.all_conditions_met).to eql "no"
+    expect(presenter.authority_to_proceed).to eql "yes"
   end
 
-  it "presents all conditions met when it has not been met (false)" do
-    project = double(Conversion::Project, all_conditions_met: false)
+  it "presents authority to proceed (all conditions met for Transfer projects) when it has not been met (nil)" do
+    project = double(Transfer::Project, all_conditions_met: nil)
 
     presenter = described_class.new(project)
 
-    expect(presenter.all_conditions_met).to eql "no"
+    expect(presenter.authority_to_proceed).to eql "no"
+  end
+
+  it "presents authority to proceed (all conditions met for Transfer projects) when it has not been met (false)" do
+    project = double(Transfer::Project, all_conditions_met: false)
+
+    presenter = described_class.new(project)
+
+    expect(presenter.authority_to_proceed).to eql "no"
   end
 
   it "presents all risk protection arrangement when it is standard" do
