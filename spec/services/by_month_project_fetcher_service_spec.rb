24cec2714e0d7a0ad9feb871afdda6b7051b8195
Laura Porter <laura@dxw.com>
2024-01-31 15:46:49 +0000

    Add services to fetch projects by date range
    Now that we have a `Project.significant_date_in_range` scope, we can use it
    in the existing ByMonthProjectFetcherService to fetch conversions and transfers
    with a confirmed significant date in a desired range.

--- spec/services/by_month_project_fetcher_service_spec.rb
+++ spec/services/by_month_project_fetcher_service_spec.rb
@@ -145,4 +145,54 @@ RSpec.describe ByMonthProjectFetcherService do
       expect(academies_api_pre_fetcher).to have_received(:call!).once
     end
   end
+
+  describe "#conversion_projects_by_date_range" do
+    context "with pre fetching disabled for this test" do
+      before do
+        mock_all_academies_api_responses
+      end
+
+      it "returns all conversion projects with a confirmed date in the desired range, and orders by significant date" do
+        january = Date.parse("2023-1-1")
+        february = Date.parse("2023-2-1")
+        march = Date.parse("2023-3-1")
+
+        project_1 = create(:conversion_project, significant_date_provisional: false, significant_date: january)
+        project_2 = create(:conversion_project, significant_date_provisional: false, significant_date: february)
+        project_3 = create(:conversion_project, significant_date_provisional: false, significant_date: march)
+
+        create(:date_history, project: project_1, previous_date: january, revised_date: january)
+        create(:date_history, project: project_2, previous_date: february, revised_date: march)
+        create(:date_history, project: project_3, previous_date: march, revised_date: march)
+
+        projects_fetcher = described_class.new
+        expect(projects_fetcher.conversion_projects_by_date_range("2023-1-1", "2023-3-1")).to eq([project_1, project_2, project_3])
+      end
+    end
+  end
+
+  describe "#transfer_projects_by_date_range" do
+    context "with pre fetching disabled for this test" do
+      before do
+        mock_all_academies_api_responses
+      end
+
+      it "returns all transfer projects with a confirmed date in the desired range, and orders by significant date" do
+        january = Date.parse("2023-1-1")
+        february = Date.parse("2023-2-1")
+        march = Date.parse("2023-3-1")
+
+        project_1 = create(:transfer_project, significant_date_provisional: false, significant_date: january)
+        project_2 = create(:transfer_project, significant_date_provisional: false, significant_date: february)
+        project_3 = create(:transfer_project, significant_date_provisional: false, significant_date: march)
+
+        create(:date_history, project: project_1, previous_date: january, revised_date: january)
+        create(:date_history, project: project_2, previous_date: february, revised_date: march)
+        create(:date_history, project: project_3, previous_date: march, revised_date: march)
+
+        projects_fetcher = described_class.new
+        expect(projects_fetcher.transfer_projects_by_date_range("2023-1-1", "2023-3-1")).to eq([project_1, project_2, project_3])
+      end
+    end
+  end
 end
