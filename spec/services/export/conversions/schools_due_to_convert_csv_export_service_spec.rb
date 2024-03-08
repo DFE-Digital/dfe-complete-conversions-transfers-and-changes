6ccfe8d33d4ade2c530775cc5b42731ec2e79357
Laura Porter <laura@dxw.com>
2024-03-08 15:43:07 +0000

    Create an export for Schools due to convert
    We are going to merge the ESFA & Grant management and Finance exports into one
    bigger export.
    
    The name of this export is not "schools due to convert" in the UI, but because
    we already have an "academies due to transfer" export, the naming of this export
    as "schools due to convert" makes sense.
    
    This export is more or less every column of data we collect for conversion
    projects.
    
    We haven't had to add any more presenters, it reuses all existing presenters.

---
+++ spec/services/export/conversions/schools_due_to_convert_csv_export_service_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+RSpec.describe Export::Conversions::SchoolsDueToConvertCsvExportService do
+  describe "#call" do
+    it "returns only the headers when there are no projects" do
+      projects = []
+
+      csv_export = described_class.new(projects).call
+
+      expect(csv_export).to include("School URN")
+      expect(csv_export).to include("Assigned to email")
+    end
+
+    it "returns a row of data" do
+      mock_all_academies_api_responses
+      mock_successful_members_api_responses(member_name: build(:members_api_name), member_contact_details: [build(:members_api_contact_details)])
+      project = build(:conversion_project)
+      projects = [project]
+
+      csv_export = described_class.new(projects).call
+
+      expect(csv_export).to include(project.establishment.name)
+      expect(csv_export).to include(project.urn.to_s)
+      expect(csv_export).to include(project.assigned_to.email)
+    end
+  end
+end
