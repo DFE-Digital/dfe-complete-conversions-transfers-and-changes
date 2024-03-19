e25f45528f90d1f0049010534e965aa565eebe3a
Laura Porter <laura@dxw.com>
2024-03-19 09:32:48 +0000

    Remove the ESFA and Funding agreement letters exports
    Now that we have the "mega" export for ESFA, RPA and funding agreement letters
    (the "all schools converting" export) we no longer need these individualised
    exports.

--- config/routes.rb
+++ config/routes.rb
@@ -157,20 +157,6 @@ Rails.application.routes.draw do
           end
           namespace :export do
             get "/", to: "projects#index"
-            namespace :funding_agreement_letters, path: "funding-agreement-letters" do
-              namespace :conversions do
-                get "/", to: "projects#index"
-                get ":month/:year", to: "projects#show", constraints: {month: MONTH_1_12_REGEX, year: YEAR_2000_2499_REGEX}, as: :show
-                get ":month/:year/csv", to: "projects#csv", constraints: {month: MONTH_1_12_REGEX, year: YEAR_2000_2499_REGEX}, as: :csv
-              end
-            end
-            namespace :education_and_skills_funding_agency, path: "esfa" do
-              namespace :conversions do
-                get "/", to: "projects#index"
-                get ":month/:year", to: "projects#show", constraints: {month: MONTH_1_12_REGEX, year: YEAR_2000_2499_REGEX}, as: :show
-                get ":month/:year/csv", to: "projects#csv", constraints: {month: MONTH_1_12_REGEX, year: YEAR_2000_2499_REGEX}, as: :csv
-              end
-            end
             namespace :grant_management_and_finance_unit, path: "grant-management-and-finance-unit" do
               namespace :conversions do
                 get "/", to: "projects#index"
