8288573b34e3987224102555e62f4131528a395a
Laura Porter <laura@dxw.com>
2024-03-22 12:45:33 +0000

    Remove the Funding Agreement Contact task
    This task is not used in the task list and all its tests are skipped. Remove it
    for now - we can re-add it if it's needed later.

--- spec/features/conversions/users_can_complete_conversion_tasks_spec.rb
+++ spec/features/conversions/users_can_complete_conversion_tasks_spec.rb
@@ -117,34 +117,6 @@ RSpec.feature "Users can complete conversion tasks" do
     end
   end
 
-  context "the funding agreement letters task" do
-    let(:project) { create(:conversion_project, assigned_to: user) }
-
-    context "when the project has contacts already" do
-      let!(:contact) { create(:project_contact, project: project) }
-
-      skip "lets the user select an existing contact" do
-        visit project_tasks_path(project)
-        click_on "Confirm who will get the funding agreement letters"
-        choose contact.name
-        click_on I18n.t("task_list.continue_button.text")
-
-        expect(project.reload.funding_agreement_contact_id).to eq contact.id
-      end
-    end
-
-    context "when the project has no contacts" do
-      skip "directs the user to add contacts" do
-        visit project_tasks_path(project)
-        click_on "Confirm who will get the funding agreement letters"
-
-        expect(page).to have_content("Add contacts")
-        click_link "add a contact"
-        expect(page.current_path).to include("external-contacts")
-      end
-    end
-  end
-
   context "the main contact task" do
     let(:project) { create(:conversion_project, assigned_to: user) }
 
