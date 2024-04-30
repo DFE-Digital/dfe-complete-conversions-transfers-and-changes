3b10c2d275cd9caf08eaaf44454e066667d4f794
Laura Porter <laura@dxw.com>
2024-04-30 11:44:15 +0100

    Remove Confirmed & Revised service methods
    When we moved the All projects -> By month views to be split by Conversions &
    Transfers, and not Confirmed date / Revised date, these methods became obsolete.

--- app/services/by_month_project_fetcher_service.rb
+++ app/services/by_month_project_fetcher_service.rb
@@ -3,42 +3,6 @@ class ByMonthProjectFetcherService
     @pre_fetch_academies_api = pre_fetch_academies_api
   end
 
-  def confirmed(month, year)
-    projects = Project.confirmed.filtered_by_significant_date(month, year)
-
-    AcademiesApiPreFetcherService.new.call!(projects) if @pre_fetch_academies_api
-    sort_by_conditions_met_and_name(projects)
-  end
-
-  def revised(month, year)
-    projects = Project.significant_date_revised_from(month, year)
-
-    AcademiesApiPreFetcherService.new.call!(projects) if @pre_fetch_academies_api
-    sort_by_conditions_met_and_name(projects)
-  end
-
-  def confirmed_openers_by_team(month, year, team)
-    projects = if team.eql?("regional_casework_services")
-      Project.assigned_to_regional_caseworker_team.confirmed.filtered_by_significant_date(month, year)
-    else
-      Project.not_assigned_to_regional_caseworker_team.by_region(team).confirmed.filtered_by_significant_date(month, year)
-    end
-
-    AcademiesApiPreFetcherService.new.call!(projects) if @pre_fetch_academies_api
-    sort_by_conditions_met_and_name(projects)
-  end
-
-  def revised_openers_by_team(month, year, team)
-    projects = if team.eql?("regional_casework_services")
-      Project.assigned_to_regional_caseworker_team.significant_date_revised_from(month, year)
-    else
-      Project.not_assigned_to_regional_caseworker_team.by_region(team).significant_date_revised_from(month, year)
-    end
-
-    AcademiesApiPreFetcherService.new.call!(projects) if @pre_fetch_academies_api
-    sort_by_conditions_met_and_name(projects)
-  end
-
   def conversion_projects_by_date_range(from_date, to_date)
     projects = Project.conversions.significant_date_in_range(from_date, to_date)
 
