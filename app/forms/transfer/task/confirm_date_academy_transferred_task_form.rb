cbb89e0d5d456372f6e9cb9d4178fa092302107d
meyric <meyric.rawlings@education.gov.uk>
2024-05-14 09:05:54 +0100

    Update confirm transfer date validation
    We want to take the same approach to all our date validation, using the
    `GovukDateFieldParameters` class before assignment takes place.

--- app/forms/transfer/task/confirm_date_academy_transferred_task_form.rb
+++ app/forms/transfer/task/confirm_date_academy_transferred_task_form.rb
@@ -1,12 +1,10 @@
 class Transfer::Task::ConfirmDateAcademyTransferredTaskForm < BaseOptionalTaskForm
-  attribute :date_transferred, :date
-  attribute "date_transferred(1i)"
-  attribute "date_transferred(2i)"
-  attribute "date_transferred(3i)"
+  include ActiveRecord::AttributeAssignment
 
-  validate :govuk_three_field_date_format
+  attribute :date_transferred, :date
 
   def initialize(tasks_data, user)
+    @date_param_errors = ActiveModel::Errors.new(self)
     @tasks_data = tasks_data
     @user = user
     @project = tasks_data.project
@@ -15,51 +13,30 @@ class Transfer::Task::ConfirmDateAcademyTransferredTaskForm < BaseOptionalTaskFo
     self.date_transferred = @project.tasks_data.confirm_date_academy_transferred_date_transferred
   end
 
+  def assign_attributes(attributes)
+    if GovukDateFieldParameters.new(:date_transferred, attributes).invalid?
+      @date_param_errors.add(:date_transferred, I18n.t("transfer.task.confirm_date_academy_transferred.errors.invalid"))
+
+      attributes.delete("date_transferred(3i)")
+      attributes.delete("date_transferred(2i)")
+      attributes.delete("date_transferred(1i)")
+    end
+
+    super(attributes)
+  end
+
+  def valid?(context = nil)
+    super(context)
+    errors.merge!(@date_param_errors)
+    errors.empty?
+  end
+
   def save
     if valid?
       @tasks_data.assign_attributes(
-        confirm_date_academy_transferred_date_transferred: formatted_date
+        confirm_date_academy_transferred_date_transferred: date_transferred
       )
       @tasks_data.save!
     end
-  end
-
-  def formatted_date
-    return nil if month.blank? || year.blank? || day.blank?
-    Date.new(year, month, day)
-  end
-
-  private def govuk_three_field_date_format
-    return if month.blank? && year.blank? && day.blank?
-
-    Date.new(year, month, day)
-  rescue TypeError, Date::Error
-    errors.add(:date_transferred, I18n.t("transfer.task.confirm_date_academy_transferred.errors.format"))
-  end
-
-  private def day
-    day = attributes["date_transferred(3i)"]
-    return if day.blank?
-
-    day.to_i
-  end
-
-  private def month
-    month = attributes["date_transferred(2i)"]
-    return if month.blank?
-
-    month.to_i
-  end
-
-  private def year
-    year = attributes["date_transferred(1i)"]
-    return false unless ("1900".."3000").to_a.include?(year)
-    return if year.blank?
-
-    year.to_i
-  end
-
-  def completed?
-    date_transferred.present?
   end
 end
