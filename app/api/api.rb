90723b2119a0f209e7160d6ec31ff2a5e363ba9e
meyric <meyric.rawlings@education.gov.uk>
2024-05-15 12:07:13 +0100

    Add and configure Grape Swagger
    This gem will allow us to document the API as we build it and then
    expose that documentation via the Swagger UI.
    
    This is a very simple starting point, we enable the documentation and
    set the path to `/api/swagger`.

--- app/api/api.rb
+++ app/api/api.rb
@@ -4,4 +4,10 @@ class Api < Grape::API
   prefix "api"
   format :json
   mount V1::Api
+
+  add_swagger_documentation \
+    info: {
+      title: "Complete conversions, transfers and changes API"
+    },
+    mount_path: "/swagger"
 end
