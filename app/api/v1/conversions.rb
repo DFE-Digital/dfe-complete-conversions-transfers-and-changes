09e2794205b8528356ff1ab692c996bedd86d5a1
Laura Porter <laura@dxw.com>
2024-05-21 15:55:35 +0100

    Add an endpoint `/form-a-mat` for creating Form a MAT projects

--- app/api/v1/conversions.rb
+++ app/api/v1/conversions.rb
@@ -28,6 +28,37 @@ class V1::Conversions < Grape::API
       rescue Api::Conversions::CreateProjectService::ProjectCreationError => e
         {error: e.message}
       end
+
+      resource "form-a-mat" do
+        params do
+          requires :conversion_project, type: Hash do
+            requires :urn, type: Integer
+            requires :new_trust_reference_number, type: String
+            requires :new_trust_name, type: String
+            requires :advisory_board_date, type: Date
+            requires :advisory_board_conditions, type: String
+            requires :provisional_conversion_date, type: Date
+            requires :directive_academy_order, type: Boolean
+            requires :created_by_email, type: String
+            requires :created_by_first_name, type: String
+            requires :created_by_last_name, type: String
+          end
+        end
+
+        desc "Create a form a MAT conversion project"
+        post "/" do
+          authenticate!
+
+          project_params = declared(params)[:conversion_project]
+
+          service = Api::Conversions::CreateProjectService.new(project_params)
+          project = service.call
+
+          {conversion_project: Rails.application.routes.url_helpers.project_information_path(project)}
+        rescue Api::Conversions::CreateProjectService::ProjectCreationError => e
+          {error: e.message}
+        end
+      end
     end
   end
 end
--- spec/api/v1/conversions_spec.rb
+++ spec/api/v1/conversions_spec.rb
@@ -124,4 +124,96 @@ RSpec.describe V1::Conversions do
       end
     end
   end
+
+  describe "post form-a-mat/" do
+    context "when there is a valid api key in the header" do
+      before do
+        ApiKey.create(api_key: "testkey", expires_at: Date.tomorrow)
+        mock_successful_api_response_to_create_any_project
+      end
+
+      let(:regional_delivery_officer) { create(:regional_delivery_officer_user) }
+
+      context "when all required params are present" do
+        context "and the required params are valid" do
+          it "creates a new Form a MAT project" do
+            post "/api/v1/projects/conversions/form-a-mat",
+              params: {
+                conversion_project: {
+                  urn: 121813,
+                  new_trust_reference_number: "TR12345",
+                  new_trust_name: "The New Trust",
+                  advisory_board_date: "2024-1-1",
+                  advisory_board_conditions: "Some conditions",
+                  provisional_conversion_date: "2025-1-1",
+                  directive_academy_order: true,
+                  created_by_email: regional_delivery_officer.email,
+                  created_by_first_name: regional_delivery_officer.first_name,
+                  created_by_last_name: regional_delivery_officer.last_name
+                }
+              },
+              as: :json,
+              headers: {Apikey: "testkey"}
+
+            project = Project.last
+            expect(response.body).to eq({conversion_project: "/projects/#{project.id}/information"}.to_json)
+          end
+        end
+
+        context "but the new Trust Reference Number is not valid" do
+          it "returns an error" do
+            post "/api/v1/projects/conversions/form-a-mat",
+              params: {
+                conversion_project: {
+                  urn: 121813,
+                  new_trust_reference_number: "12345",
+                  new_trust_name: "The New Trust",
+                  advisory_board_date: "2024-1-1",
+                  advisory_board_conditions: "Some conditions",
+                  provisional_conversion_date: "2025-1-1",
+                  directive_academy_order: true,
+                  created_by_email: regional_delivery_officer.email,
+                  created_by_first_name: regional_delivery_officer.first_name,
+                  created_by_last_name: regional_delivery_officer.last_name
+                }
+              },
+              as: :json,
+              headers: {Apikey: "testkey"}
+
+            expect(response.body).to eq({error: "New trust reference number The Trust reference number must be 'TR' followed by 5 numbers, e.g. TR01234"}.to_json)
+          end
+        end
+      end
+
+      context "when any required params are missing" do
+        it "returns an error" do
+          post "/api/v1/projects/conversions/form-a-mat", headers: {Apikey: "testkey"}
+          expect(response.body).to include("conversion_project is missing")
+        end
+      end
+
+      context "when the 'regular' conversion parameters are posted to the Form a MAT endpoint" do
+        it "returns an error" do
+          post "/api/v1/projects/conversions/form-a-mat",
+            params: {
+              conversion_project: {
+                urn: 121813,
+                incoming_trust_ukprn: 10066123,
+                advisory_board_date: "2024-1-1",
+                advisory_board_conditions: "Some conditions",
+                provisional_conversion_date: "2025-1-1",
+                directive_academy_order: true,
+                created_by_email: regional_delivery_officer.email,
+                created_by_first_name: regional_delivery_officer.first_name,
+                created_by_last_name: regional_delivery_officer.last_name
+              }
+            },
+            as: :json,
+            headers: {Apikey: "testkey"}
+
+          expect(response.body).to eq({error: "conversion_project[new_trust_reference_number] is missing, conversion_project[new_trust_name] is missing"}.to_json)
+        end
+      end
+    end
+  end
 end
