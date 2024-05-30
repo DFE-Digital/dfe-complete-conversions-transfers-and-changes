b819af48775259e90b6bc41251daca1e046f3290
meyric <meyric.rawlings@education.gov.uk>
2024-05-30 13:03:48 +0100

    Create export form objects
    We are making the csv exports more conventional and for users to supply
    a date range for the exported projects.
    
    We start with the RPA, SUG and FA letters export, first creating a base
    class that we can share across all the exports later.
    
    The form accepts a from and to date and returns the appropriate csv
    export using the existing service and scopes.

---
+++ app/forms/export/new_rpa_sug_and_fa_letters_form.rb
@@ -0,0 +1,8 @@
+class Export::NewRpaSugAndFaLettersForm < Export::BaseForm
+  def export
+    projects = Project.conversions.significant_date_in_range(from_date.to_s, to_date.to_s)
+    pre_fetched_projects = AcademiesApiPreFetcherService.new.call!(projects)
+
+    Export::Conversions::SchoolsDueToConvertCsvExportService.new(pre_fetched_projects).call
+  end
+end
