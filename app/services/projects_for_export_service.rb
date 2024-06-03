2ac6f5fcf625d4b8c72b9dfd5704996e13f42f43
meyric <meyric.rawlings@education.gov.uk>
2024-06-03 12:35:21 +0100

    Remove old academies due to transfer export code
    We have updated how we offer this export, all of this is now redundant.

--- app/services/projects_for_export_service.rb
+++ app/services/projects_for_export_service.rb
@@ -3,9 +3,4 @@ class ProjectsForExportService
     projects = Transfer::Project.filtered_by_advisory_board_date(month, year)
     AcademiesApiPreFetcherService.new.call!(projects)
   end
-
-  def transfer_projects_by_significant_date(month:, year:)
-    projects = Transfer::Project.filtered_by_significant_date(month, year)
-    AcademiesApiPreFetcherService.new.call!(projects)
-  end
 end
