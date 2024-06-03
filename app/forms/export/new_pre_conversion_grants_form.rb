83081c3ae2265770364dc8a7d65aba200552c381
meyric <meyric.rawlings@education.gov.uk>
2024-06-03 08:43:46 +0100

    Add pre conversion grants export form
    We're making all the exports as conventional as possible, this work
    adds a form object to handle the submission of a from and to date for
    the pre conversion grants export.

---
+++ app/forms/export/new_pre_conversion_grants_form.rb
@@ -0,0 +1,8 @@
+class Export::NewPreConversionGrantsForm < Export::BaseForm
+  def export
+    projects = Project.conversions.advisory_board_date_in_range(from_date.to_s, to_date.to_s)
+    pre_fetched_projects = AcademiesApiPreFetcherService.new.call!(projects)
+
+    Export::Conversions::GrantManagementAndFinanceUnitCsvExportService.new(pre_fetched_projects).call
+  end
+end
