3cdecde92e52b19c6ca022da644ba57c7a256d3e
meyric <meyric.rawlings@education.gov.uk>
2024-06-11 14:40:17 +0100

    Fix the order of SignificantDateHistory reasons
    Right now there is no specified order for the reasons stored against a
    change of date history, but we should set something so that we can rely
    on the order, in tests for example.
    
    We've settled on alphabetical by reason type as a starting point.

--- app/models/significant_date_history.rb
+++ app/models/significant_date_history.rb
@@ -2,7 +2,7 @@ class SignificantDateHistory < ApplicationRecord
   belongs_to :project, class_name: "Project"
   belongs_to :user
   has_one :note, dependent: :destroy, foreign_key: :significant_date_history_id
-  has_many :reasons, dependent: :destroy, foreign_key: :significant_date_history_id, class_name: "SignificantDateHistoryReason"
+  has_many :reasons, -> { order :reason_type }, dependent: :destroy, foreign_key: :significant_date_history_id, class_name: "SignificantDateHistoryReason"
 
   validates :previous_date, :revised_date, presence: true
 end
