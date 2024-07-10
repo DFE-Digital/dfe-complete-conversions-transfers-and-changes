f1a7c0410eaca159bef4c6fa2967c6806be06caa
Laura Porter <laura@dxw.com>
2024-07-10 16:18:11 +0100

    We no longer search for MPs for a project by constituency
    Since the constituency boundaries changed on 31/5/24, the parliamentary
    constituencies and constituencies associated with schools in GIAS have fallen
    out of alignment. Therefore searching for an MP by a school's constituency will
    no longer be accurate. Remove all these methods and use the postcode search
    instead.

--- app/models/api/members_api/client.rb
+++ app/models/api/members_api/client.rb
@@ -11,32 +11,6 @@ class Api::MembersApi::Client
     @connection = connection || default_connection
   end
 
-  def constituency(search_term)
-    response = constituency_search(search_term)
-
-    case response.status
-    when 200
-      body = JSON.parse(response.body)
-      Result.new(body, nil)
-    else
-      Result.new(nil, Error.new(I18n.t("members_api.errors.other", search_term: search_term, status: response.status)))
-    end
-  end
-
-  def member_for_constituency(constituency)
-    member = member_id(constituency)
-
-    return Result.new(nil, member.error) if member.error.present?
-
-    member_id = member.object
-    member_name = member_name(member_id).object
-    contact_details = member_contact_details(member_id).object.find { |details| details.type_id == 1 }
-
-    Result.new(Api::MembersApi::MemberDetails.new(member_name, contact_details), nil)
-  rescue NoMethodError
-    Result.new(nil, Error.new(I18n.t("members_api.errors.contact_details_not_found", member_id: member_id)))
-  end
-
   def member_for_postcode(postcode)
     response = postcode_search(postcode)
 
@@ -62,35 +36,6 @@ class Api::MembersApi::Client
     Result.new(Api::MembersApi::MemberDetails.new(member_name, contact_details), nil)
   end
 
-  def member_id(search_term)
-    constituency_data = constituency(search_term)
-    if constituency_data.error.present?
-      return Result.new(nil, constituency_data.error)
-    end
-
-    if constituency_data.object["items"].count == 0
-      Result.new(nil, NotFoundError.new(NotFoundError.new(I18n.t("members_api.errors.search_term_not_found", constituency: search_term))))
-    elsif constituency_data.object["items"].count > 1
-      Result.new(nil, MultipleResultsError.new(I18n.t("members_api.errors.multiple", search_term: search_term)))
-    else
-      Result.new(member_id_from_constituency(constituency_data), nil)
-    end
-  end
-
-  def member_name(member_id)
-    response = member(member_id)
-
-    case response.status
-    when 200
-      result = JSON.parse(response.body)["value"]
-      Result.new(Api::MembersApi::MemberName.new.from_hash(result), nil)
-    when 404
-      Result.new(nil, NotFoundError.new(I18n.t("members_api.errors.member_not_found", member_id: member_id)))
-    else
-      Result.new(nil, Error.new(I18n.t("members_api.errors.other")))
-    end
-  end
-
   def member_contact_details(member_id)
     response = member_contact(member_id)
 
@@ -106,25 +51,8 @@ class Api::MembersApi::Client
     end
   end
 
-  private def constituency_search(search_term)
-    @connection.get("/api/Location/Constituency/Search", {searchText: search_term})
-  rescue Faraday::Error => error
-    raise Error.new(error)
-  end
-
   private def postcode_search(search_term)
     @connection.get("/api/Members/Search", {Location: search_term, House: 1, IsCurrentMember: true})
-  rescue Faraday::Error => error
-    raise Error.new(error)
-  end
-
-  private def member_id_from_constituency(constituency_data)
-    constituency = constituency_data.object["items"][0]["value"]
-    constituency.dig("currentRepresentation", "member", "value", "id")
-  end
-
-  private def member(member_id)
-    @connection.get("/api/Members/#{member_id}")
   rescue Faraday::Error => error
     raise Error.new(error)
   end
