2ce0431f108d6ea44a5ac03d3ecff2d44f032c07
meyric <meyric.rawlings@education.gov.uk>
2024-07-18 16:16:38 +0100

    Update trust project fetcher
    We recently removed the default scope that stopped deleted projects
    appearing so we have to update this fetcher to not include them.
    
    We can change to the `active` scope here as that excludes projects that
    are:
    
    - deleted
    - completed
    - dao_revoked

--- app/services/by_trust_project_fetcher_service.rb
+++ app/services/by_trust_project_fetcher_service.rb
@@ -12,7 +12,7 @@ class ByTrustProjectFetcherService
   end
 
   private def projects_by_trust
-    projects = Project.not_form_a_mat.not_completed
+    projects = Project.active.not_form_a_mat
     return false unless projects.any?
 
     projects.group_by(&:incoming_trust_ukprn)
