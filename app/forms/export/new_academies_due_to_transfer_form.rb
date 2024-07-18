7536f369f8f7db193c7842da2a0e3694f8d88d27
meyric <meyric.rawlings@education.gov.uk>
2024-07-18 15:01:24 +0100

    Ensure exports do not include deleted projects
    We removed the default scope on Projects so we have to make sure that
    exports do not include them now.
    
    We do this by using the project `state` enums generated scopes.
    
    We've added explicit test cases to help us be clear which projects are
    included.

--- app/forms/export/new_academies_due_to_transfer_form.rb
+++ app/forms/export/new_academies_due_to_transfer_form.rb
@@ -1,6 +1,6 @@
 class Export::NewAcademiesDueToTransferForm < Export::BaseForm
   def export
-    projects = Project.transfers.significant_date_in_range(from_date.to_s, to_date.to_s)
+    projects = Project.not_deleted.transfers.significant_date_in_range(from_date.to_s, to_date.to_s)
     pre_fetched_projects = AcademiesApiPreFetcherService.new.call!(projects)
 
     Export::Transfers::AcademiesDueToTransferCsvExportService.new(pre_fetched_projects).call
--- spec/forms/export/new_academies_due_to_transfer_form_spec.rb
+++ spec/forms/export/new_academies_due_to_transfer_form_spec.rb
@@ -22,12 +22,12 @@ RSpec.describe Export::NewAcademiesDueToTransferForm, type: :model do
       to_date = Date.new(2024, 4, 1)
       form = described_class.new(from_date: from_date, to_date: to_date)
 
-      allow(Project).to receive(:transfers).and_call_original
+      allow(Project).to receive(:not_deleted).and_call_original
       allow(Project).to receive(:significant_date_in_range).and_call_original
 
       form.export
 
-      expect(Project).to have_received(:transfers)
+      expect(Project).to have_received(:not_deleted)
       expect(Project).to have_received(:significant_date_in_range).with(from_date.to_s, to_date.to_s)
     end
 
@@ -43,6 +43,33 @@ RSpec.describe Export::NewAcademiesDueToTransferForm, type: :model do
 
       expect(Export::Transfers::AcademiesDueToTransferCsvExportService).to have_received(:new).once
       expect(fake_csv_export_service).to have_received(:call).once
+    end
+
+    describe "includes the right projects" do
+      before do
+        mock_all_academies_api_responses
+      end
+
+      let(:from_date) { Date.new(2024, 1, 1) }
+      let(:to_date) { Date.new(2024, 4, 1) }
+
+      let!(:active_project) { create(:transfer_project, :active, transfer_date: Date.new(2024, 2, 1), urn: 111111) }
+      let!(:completed_project) { create(:transfer_project, :completed, transfer_date: Date.new(2024, 2, 1), urn: 222222) }
+      let!(:deleted_project) { create(:transfer_project, :deleted, transfer_date: Date.new(2024, 2, 1), urn: 333333) }
+
+      subject { described_class.new(from_date: from_date, to_date: to_date).export }
+
+      it "includes active projects" do
+        expect(subject).to include active_project.urn.to_s
+      end
+
+      it "includes completed projects" do
+        expect(subject).to include completed_project.urn.to_s
+      end
+
+      it "does not include deleted projects" do
+        expect(subject).not_to include deleted_project.urn.to_s
+      end
     end
   end
 end
