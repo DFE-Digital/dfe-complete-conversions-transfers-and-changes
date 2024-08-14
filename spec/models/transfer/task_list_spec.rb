da7137f3dbbeef81f45ed776aef4746681801f76
meyric <meyric.rawlings@education.gov.uk>
2024-08-14 08:53:52 +0100

    Add confirm headteacher contact to transfers
    Just like conversions, we want to explicitly ask the user to confirm
    which contact is the headteacher. The information is important to
    downstream data users.
    
    This work is a repeat of the conversions work.

--- spec/models/transfer/task_list_spec.rb
+++ spec/models/transfer/task_list_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe Transfer::TaskList do
       transfer_task_list_identifiers = [
         :handover,
         :stakeholder_kick_off,
+        :confirm_headteacher_contact,
         :main_contact,
         :request_new_urn_and_record,
         :sponsored_support_grant,
@@ -53,7 +54,7 @@ RSpec.describe Transfer::TaskList do
       project = create(:transfer_project)
       task_list = described_class.new(project, user)
 
-      expect(task_list.tasks.count).to eql 25
+      expect(task_list.tasks.count).to eql 26
       expect(task_list.tasks.first).to be_a Transfer::Task::HandoverTaskForm
       expect(task_list.tasks.last).to be_a Transfer::Task::DeclarationOfExpenditureCertificateTaskForm
     end
