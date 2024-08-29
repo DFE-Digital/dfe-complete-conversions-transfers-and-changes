bbd836c5f23ce49fcbb65e56f5ed29d63f78b276
meyric <meyric.rawlings@education.gov.uk>
2024-08-29 09:48:49 +0100

    Update the govuk_markdown gem
    Whilst testing a Rails update, we found the new version of this gem
    requires a slight change to our custom renderer.
    
    We are just passing the new `govuk_options` in.

--- config/initializers/govuk_markdown.rb
+++ config/initializers/govuk_markdown.rb
@@ -1,6 +1,6 @@
 module GovukMarkdown
-  def self.render(markdown)
-    renderer = GovukMarkdown::Renderer.new(with_toc_data: true, link_attributes: {class: "govuk-link", target: "_blank"})
+  def self.render(markdown, govuk_options = {})
+    renderer = GovukMarkdown::Renderer.new(govuk_options, {with_toc_data: true, link_attributes: {class: "govuk-link", target: "_blank"}})
     Redcarpet::Markdown.new(renderer, tables: true, no_intra_emphasis: true).render(markdown).strip
   end
 end
