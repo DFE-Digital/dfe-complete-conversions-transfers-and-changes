5473cbd929145d676d6fec4b683ba6b2d61de4b8
meyric <meyric.rawlings@education.gov.uk>
2024-09-02 12:23:17 +0100

    Fix unknown task identifiers
    When loading a task into the edit view it was previously possible to put
    in any string as the path and raise an error as the controller tries to
    load the class with the string.
    
    This work adds an error when the path cannot be turned into a Task class
    and `rescues_from` from the error with a 404 page.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
 ## [Unreleased][unreleased]
 
+### Fixed
+
+- unknown task identifier paths now show a 'page not found' error.
+
 ## [Release-85][release-85]
 
 ### Changed
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,6 +4,10 @@ class TasksController < ApplicationController
   before_action :find_tasks_data
   before_action :find_task, :find_task_notes, except: :index
 
+  class TaskKlassNotFoundError < StandardError; end
+
+  rescue_from TaskKlassNotFoundError, with: :not_found_error
+
   def index
     case @project.type
     when "Conversion::Project"
@@ -38,7 +42,10 @@ class TasksController < ApplicationController
   end
 
   private def find_task
-    @task = klass_from_path.new(@tasks_data, current_user)
+    task_klass = klass_from_path
+    raise TaskKlassNotFoundError.new(task_identifier) unless task_klass
+
+    @task = task_klass.new(@tasks_data, current_user)
   end
 
   private def task_params
@@ -55,6 +62,8 @@ class TasksController < ApplicationController
 
   private def klass_from_path
     "#{project_type}::Task::#{task_klass_identifier}TaskForm".constantize
+  rescue NameError
+    nil
   end
 
   private def task_view
