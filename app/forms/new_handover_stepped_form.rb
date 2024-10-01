167f31fdd8cbf4a3ebdb44586271b69c5f31371a
meyric <meyric.rawlings@education.gov.uk>
2024-10-01 15:18:29 +0100

    Add handover form object
    To 'handover' a project created on the API we need to add some
    attributes that do not exist in the Prepare application and so cannot be
    provided, projects added via the API are invalid until this is done.
    
    The attributes differ for conversions and transfers and the actual
    assignment to a user is different depending on the team the project is
    assigned to.
    
    As the variation is so small, we kept to this single fancy form object
    with two validation contexts. Project use single table inheritance, so
    we can assign the project type attributes regardless.
    
    The default context is used when the project is assigned to the Regional
    casework services team.
    
    The 'assign' context is used when the project is assigned to one of the
    regional teams and so requires extra attributes to be set.
    
    This essentially makes this a very small 'stepped' form with only two
    steps, which we have done before, we'll send all the attributes to the
    second step when we need to.
    
    We ensure the unused attributes are set to nil so we can simply save
    whichever Project is passed in.

---
+++ app/forms/new_handover_stepped_form.rb
@@ -0,0 +1,69 @@
+class NewHandoverSteppedForm
+  include Teamable
+  include ActiveModel::Model
+  include ActiveModel::Attributes
+
+  attribute :assigned_to_regional_caseworker_team, :boolean
+  attribute :handover_note_body, :string
+  attribute :establishment_sharepoint_link, :string
+  attribute :incoming_trust_sharepoint_link, :string
+  attribute :outgoing_trust_sharepoint_link, :string
+  attribute :two_requires_improvement, :boolean
+  attribute :team, :string
+  attribute :email, :string
+
+  validates :assigned_to_regional_caseworker_team, inclusion: {in: [true, false]}
+
+  validates :handover_note_body, presence: true
+
+  validates :establishment_sharepoint_link, presence: true, sharepoint_url: true
+  validates :incoming_trust_sharepoint_link, presence: true, sharepoint_url: true
+
+  validates :outgoing_trust_sharepoint_link, presence: true, sharepoint_url: true, if: -> { @project.is_a?(Transfer::Project) }
+
+  validates :two_requires_improvement, inclusion: {in: [true, false]}, if: -> { @project.is_a?(Conversion::Project) }
+
+  validates :team, presence: true, inclusion: {in: regional_teams}, on: :assign
+
+  validates :email, presence: true, on: :assign
+  validates :email, format: {with: /\A\S+@education.gov.uk\z/i}, if: -> { email.present? }
+  validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}, if: -> { email.present? }
+
+  def self.list_of_teams
+    regional_teams.sort.map do |team|
+      OpenStruct.new(id: team, name: I18n.t("teams.#{team}"))
+    end
+  end
+
+  def initialize(project, user, args = {})
+    @user = user
+    @project = project
+    super(args)
+  end
+
+  def save
+    @project.assign_attributes({
+      team: team || :regional_casework_services,
+      assigned_to_id: assigned_user_id,
+      assigned_at: DateTime.now,
+      establishment_sharepoint_link: establishment_sharepoint_link,
+      incoming_trust_sharepoint_link: incoming_trust_sharepoint_link,
+      outgoing_trust_sharepoint_link: outgoing_trust_sharepoint_link,
+      two_requires_improvement: two_requires_improvement,
+      state: :active
+    })
+    @project.save!
+
+    create_handover_note!
+  end
+
+  private def assigned_user_id
+    return unless email
+
+    User.find_by_email(email).id
+  end
+
+  private def create_handover_note!
+    Note.create!(body: handover_note_body, project: @project, user: @user, task_identifier: :handover)
+  end
+end
