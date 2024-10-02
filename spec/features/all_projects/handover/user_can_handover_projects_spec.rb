2908eb5e6a423283bf1dbf2c0ff9ea60b9f0ce3a
meyric <meyric.rawlings@education.gov.uk>
2024-10-02 15:56:53 +0100

    Show form a MAT projects for handover
    The only difference here is that the incoming trust is replaced by a new
    trust and reference number, everything else is the same so we've popped
    the logic into the view.

--- spec/features/all_projects/handover/user_can_handover_projects_spec.rb
+++ spec/features/all_projects/handover/user_can_handover_projects_spec.rb
@@ -29,6 +29,33 @@ RSpec.feature "Users can handover projects" do
       expect(page).to have_content("Projects to handover")
       expect(page).to have_content("These projects have been worked on in Prepare")
     end
+
+    context "when the conversion is a forming a MAT" do
+      let!(:conversion_project) {
+        create(
+          :conversion_project,
+          :form_a_mat,
+          state: :inactive, urn: 123456,
+          conversion_date: Date.new(2024, 2, 1)
+        )
+      }
+
+      scenario "they can check they have the right project and cancel if not" do
+        visit all_handover_projects_path
+        click_link "Add handover details"
+
+        expect(page).to have_content("Check you have the right project")
+        expect(page).to have_content("Conversion")
+        expect(page).to have_content(conversion_project.urn)
+        expect(page).to have_content(conversion_project.new_trust_reference_number)
+        expect(page).to have_content(conversion_project.new_trust_name)
+
+        click_link "Choose a different project"
+
+        expect(page).to have_content("Projects to handover")
+        expect(page).to have_content("These projects have been worked on in Prepare")
+      end
+    end
   end
 
   context "when the project is a transfer" do
@@ -53,6 +80,33 @@ RSpec.feature "Users can handover projects" do
 
       expect(page).to have_content("Projects to handover")
       expect(page).to have_content("These projects have been worked on in Prepare")
+    end
+
+    context "when the transfer is a forming a MAT" do
+      let!(:transfer_project) {
+        create(
+          :transfer_project,
+          :form_a_mat,
+          state: :inactive, urn: 123456,
+          transfer_date: Date.new(2024, 2, 1)
+        )
+      }
+
+      scenario "they can check they have the right project and cancel if not" do
+        visit all_handover_projects_path
+        click_link "Add handover details"
+
+        expect(page).to have_content("Check you have the right project")
+        expect(page).to have_content("Transfer")
+        expect(page).to have_content(transfer_project.urn)
+        expect(page).to have_content(transfer_project.new_trust_reference_number)
+        expect(page).to have_content(transfer_project.new_trust_name)
+
+        click_link "Choose a different project"
+
+        expect(page).to have_content("Projects to handover")
+        expect(page).to have_content("These projects have been worked on in Prepare")
+      end
     end
   end
 end
