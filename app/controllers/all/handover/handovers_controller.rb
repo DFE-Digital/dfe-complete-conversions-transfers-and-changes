acaff2c4163a9a1eca656fe1d80c77f989eb3512
meyric <meyric.rawlings@education.gov.uk>
2024-10-02 13:40:48 +0100

    Allow handovers
    There is a lot here, apologies! This is one journey with four paths and
    we are handling it all with the fancy `NewHandoverSteppedForm` object so
    it all comes at once!
    
    This delivers a basic, working path for:
    
    - handing over a conversion to Regional Casework Services
    - handing over a conversion to a user in the Regional teams
    - handing over a transfer to Regional Casework Services
    - handing over a transfer to a user in the Regional teams
    
    The approach is that we don't want users to be able to return midway
    through the steps (if they are on a path with steps) so we favour
    rendering views over new paths - that means passing all the form
    parameters to the second step and then posting them off - this feels
    okay with so few attributes to manage and we do a similar thing for Date
    Histories.
    
    The bulk of the work is in the `HandoversController` which has to know
    what type of project and render the right form views.
    
    Most of the changes here are new views and the content to go with them.
    
    We extended the `Projectable` controller concern to take care of invalid
    UUIDs - this feels neater than the constraints we have in the routes (we
    could switch everything else, but not here!).
    
    We add validation that the email address matches up with a `User`.

--- app/controllers/all/handover/handovers_controller.rb
+++ app/controllers/all/handover/handovers_controller.rb
@@ -1,11 +1,12 @@
 class All::Handover::HandoversController < ApplicationController
-  after_action :verify_authorized
+  include Projectable
   rescue_from ActiveRecord::RecordNotFound, with: :not_found_error
+
+  before_action :set_form, except: [:check, :new]
+  after_action :verify_authorized
 
   def check
     authorize Project, :handover?
-
-    @project = Project.find(params[:id])
 
     case @project.type
     when "Conversion::Project"
@@ -13,5 +14,66 @@ class All::Handover::HandoversController < ApplicationController
     when "Transfer::Project"
       render "all/handover/projects/transfer/check"
     end
+  end
+
+  def new
+    authorize Project, :handover?
+
+    @form = NewHandoverSteppedForm.new(@project, @current_user)
+
+    render new_template_path
+  end
+
+  def create
+    authorize Project, :handover?
+
+    if @form.valid?
+      case @form.assigned_to_regional_caseworker_team
+      when true
+        @form.save
+        render "all/handover/projects/assigned_regional_casework_services"
+      when false
+        render "all/handover/projects/assign"
+      end
+    else
+      render new_template_path
+    end
+  end
+
+  def assign
+    authorize Project, :handover?
+
+    if @form.valid?(:assign)
+      @form.save
+      render "all/handover/projects/assigned_region"
+    else
+      render "all/handover/projects/assign"
+    end
+  end
+
+  private def new_template_path
+    case @project.type
+    when "Conversion::Project"
+      "all/handover/projects/conversion/new"
+    when "Transfer::Project"
+      "all/handover/projects/transfer/new"
+    end
+  end
+
+  private def set_form
+    @form = NewHandoverSteppedForm.new(@project, @current_user, handover_params)
+  end
+
+  private def handover_params
+    params.require(:new_handover_stepped_form).permit(
+      :assigned_to_regional_caseworker_team,
+      :handover_note_body,
+      :establishment_sharepoint_link,
+      :incoming_trust_sharepoint_link,
+      :outgoing_trust_sharepoint_link,
+      :two_requires_improvement,
+      :email,
+      :team
+    )
   end
 end
--- spec/requests/all/handover/handovers_controller_spec.rb
+++ spec/requests/all/handover/handovers_controller_spec.rb
@@ -27,5 +27,69 @@ RSpec.describe All::Handover::HandoversController, type: :request do
         expect(response).to render_template("all/handover/projects/transfer/check")
       end
     end
+
+    context "with an invalid project UUID" do
+      it "returns not found" do
+        get "/projects/all/handover/NOT-A-UUID/check"
+
+        expect(response).to have_http_status(404)
+      end
+    end
+  end
+
+  describe "#create" do
+    context "when the form is invalid" do
+      let!(:project) { create(:conversion_project) }
+
+      it "renders the form with errors" do
+        post "/projects/all/handover/#{project.id}/new", params: invalid_params
+
+        expect(response).to render_template("all/handover/projects/conversion/new")
+        expect(response.body).to include("There is a problem")
+      end
+    end
+  end
+
+  describe "#assign" do
+    context "when the form is invalid" do
+      let!(:project) { create(:conversion_project) }
+
+      it "renders the form with errors" do
+        post "/projects/all/handover/#{project.id}/assign", params: invalid_params
+
+        expect(response).to render_template("all/handover/projects/assign")
+        expect(response.body).to include("There is a problem")
+      end
+    end
+  end
+
+  def valid_params
+    {
+      new_handover_stepped_form: {
+        assigned_to_regional_caseworker_team: false,
+        handover_note_body: "Handover note.",
+        establishment_sharepoint_link: "https://educationgovuk.sharepoint.com/establishment",
+        incoming_trust_sharepoint_link: "https://educationgovuk.sharepoint.com/incoming-trust",
+        outgoing_trust_sharepoint_link: "https://educationgovuk.sharepoint.com/outgoing-trust",
+        two_requires_improvement: true,
+        email: "test.user@education.gov.uk",
+        team: :west_midlands
+      }
+    }
+  end
+
+  def invalid_params
+    {
+      new_handover_stepped_form: {
+        assigned_to_regional_caseworker_team: nil,
+        handover_note_body: nil,
+        establishment_sharepoint_link: nil,
+        incoming_trust_sharepoint_link: nil,
+        outgoing_trust_sharepoint_link: nil,
+        two_requires_improvement: nil,
+        email: nil,
+        team: nil
+      }
+    }
   end
 end
