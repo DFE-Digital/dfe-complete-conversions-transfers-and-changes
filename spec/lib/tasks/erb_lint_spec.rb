e1d894c12a68a635c6eb1ef632da75d30162ec27
Ed Davey <ed.davey@education.gov.uk>
2024-12-03 13:07:55 +0000

    Rename erblint to erb_lint
    This in response to deprecation warnings:
    
    ```
    Calling `erblint` is deprecated, please call the renamed executable
    	- `erb_lint` instead.
    
    - The config file has been renamed to `.erb_lint.yml` and `.erb-lint.yml`
    	is deprecated. Please rename your config file to `.erb_lint.yml`.
    ```

--- spec/lib/tasks/erb_lint_spec.rb
+++ spec/lib/tasks/erb_lint_spec.rb
@@ -2,23 +2,23 @@ require "rails_helper"
 require "erb_lint/cli"
 
 RSpec.describe "ERB Lint tasks", type: :task do
-  describe "erblint" do
+  describe "erb_lint" do
     it "runs the ERB Lint cli with the correct arguments" do
       erb_linter = double(ERBLint::CLI)
       allow(ERBLint::CLI).to receive(:new).and_return(erb_linter)
       expect(erb_linter).to receive(:run).with(["--lint-all"])
 
-      Rake::Task["erblint"].execute
+      Rake::Task["erb_lint"].execute
     end
   end
 
-  describe "erblint:fix" do
+  describe "erb_lint:fix" do
     it "runs the ERB Lint cli with the correct arguments" do
       erb_linter = double(ERBLint::CLI)
       allow(ERBLint::CLI).to receive(:new).and_return(erb_linter)
       expect(erb_linter).to receive(:run).with(["--lint-all", "-a"])
 
-      Rake::Task["erblint:fix"].execute
+      Rake::Task["erb_lint:fix"].execute
     end
   end
 end
