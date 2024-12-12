16b80332eb5f2fac7955320fe794fe6d0c673109
Ed Davey <ed.davey@education.gov.uk>
2024-12-12 12:18:40 +0000

    Return 'Healthy' or 'Unhealthy' string after db test
    The other products in the programme are all returning a
    string rather than a JSON representation.
    
    We now:
    
    - conform to that convention
    - include a db connection test

--- spec/api/v1/healthcheck_spec.rb
+++ spec/api/v1/healthcheck_spec.rb
@@ -1,8 +1,21 @@
 require "rails_helper"
 
 RSpec.describe V1::Healthcheck do
-  it "returns OK" do
-    get "/api/v1/healthcheck"
-    expect(response.body).to eq({status: "OK"}.to_json)
+  context "when the db connection is up" do
+    before { allow(ActiveRecord::Base).to receive(:connected?).and_return(true) }
+
+    it "returns 'Healthy'" do
+      get "/api/v1/healthcheck"
+      expect(response.body).to eq("Healthy")
+    end
+  end
+
+  context "when the db connection is NOT up" do
+    before { allow(ActiveRecord::Base).to receive(:connected?).and_return(false) }
+
+    it "returns 'Unhealthy'" do
+      get "/api/v1/healthcheck"
+      expect(response.body).to eq("Unhealthy")
+    end
   end
 end
