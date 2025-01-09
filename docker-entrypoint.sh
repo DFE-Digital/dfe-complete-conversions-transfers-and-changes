a3825d654c0f401885cdc0f9de870b59fe511fa8
Ed Davey <ed.davey@education.gov.uk>
2025-01-09 18:03:06 +0000

    Add rake task to add Project urn trigger on CI build

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,6 +8,15 @@ setup_database()
   echo "ENTRYPOINT: Running rake db:prepare…"
   # Migrate the database and if one doesn't exist then create one
   bundle exec rake db:prepare
+
+  # Manually add our EnforceUniquenessOfProjectUrn trigger
+  # In theory this trigger should be included in structure.sql if
+  # we opt to use that instead of schema.rb. But it isn't and in
+  # any case trying to load the structure from stucture.sql with TinyTDS
+  # fails due to lack of escaping on the "key# keyword....
+  echo "ENTRYPOINT: Running rake db:add_enforce_uniqueness_of_project_urn_trigger…"
+  bundle exec rake db:add_enforce_uniqueness_of_project_urn_trigger
+
   echo "ENTRYPOINT: Finished database setup."
 }
 
