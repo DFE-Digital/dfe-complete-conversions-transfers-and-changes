52abd3e2750d8bc6819881458878fdf4f8758a27
Ed Davey <ed.davey@education.gov.uk>
2025-01-13 17:35:03 +0000

    Track authenticated user in AppInsights
    We use the email address to identify the user. To achieve this we:
    
    - set the email of the "current user" in a hidden input in the DOM,
      defaulting to "Anonymous" if there is no logged-in user
    
    - extract this email from the DOM when initialising AppInsights
      (we now need to wait for the DOM to be ready/loaded)
    
    - set `appInsights.setAuthenticatedUserContext(currentUserEmail(), null, true)`
      see https://github.com/microsoft/ApplicationInsights-JS/blob/master/API-reference.md#setauthenticatedusercontext
      for details

--- app/javascript/application-insights.js
+++ app/javascript/application-insights.js
@@ -1,24 +1,33 @@
 import { ApplicationInsights } from '@microsoft/applicationinsights-web'
 import { ClickAnalyticsPlugin } from '@microsoft/applicationinsights-clickanalytics-js'
 
-const applicationInsightsConnectionString = document.head.querySelector('meta[name="application-insights-connection"]').content
+window.addEventListener('DOMContentLoaded', () => {
+  const applicationInsightsConnectionString = document.head.querySelector('meta[name="application-insights-connection"]').content
 
-const clickPluginInstance = new ClickAnalyticsPlugin()
-const clickPluginConfig = {
-  autoCapture: true
-}
-
-const appInsights = new ApplicationInsights({
-  config: {
-    connectionString: applicationInsightsConnectionString,
-    autoTrackPageVisitTime: true
-  },
-  extensions: [clickPluginInstance],
-  extensionConfig: {
-    [clickPluginInstance.identifier]: clickPluginConfig
+  const clickPluginInstance = new ClickAnalyticsPlugin()
+  const clickPluginConfig = {
+    autoCapture: true
   }
-}
-)
 
-appInsights.loadAppInsights()
-appInsights.trackPageView()
+  const appInsights = new ApplicationInsights({
+    config: {
+      connectionString: applicationInsightsConnectionString,
+      autoTrackPageVisitTime: true
+    },
+    extensions: [clickPluginInstance],
+    extensionConfig: {
+      [clickPluginInstance.identifier]: clickPluginConfig
+    }
+  }
+  )
+
+  const currentUserIdentifier = () => {
+    const currentUser = document.querySelector('#current-user')
+
+    return currentUser.dataset.identifier
+  }
+
+  appInsights.loadAppInsights()
+  appInsights.setAuthenticatedUserContext(currentUserIdentifier(), null, true)
+  appInsights.trackPageView()
+})
