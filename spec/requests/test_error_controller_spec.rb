9389740186c73df9f94a432ed204daa3cc8a9f6c
Ed Davey <ed.davey@education.gov.uk>
2025-01-16 09:27:04 +0000

    Verify that only authenticated users can trigger a test error

--- spec/requests/test_error_controller_spec.rb
+++ spec/requests/test_error_controller_spec.rb
@@ -2,14 +2,24 @@ require "rails_helper"
 
 RSpec.describe TestErrorController, type: :request do
   describe "GET to #create" do
-    before do
-      sign_in_with(create(:user))
-    end
+    context "when authenticated" do
+      before do
+        sign_in_with(create(:user))
+      end
 
-    it "raises a _TestError_" do
+      it "raises a _TestError_" do
+        expect {
+          get test_error_path
+        }.to raise_error(TestError)
+      end
+    end
+  end
+
+  context "when NOT authenticated" do
+    it "does NOT raise an error" do
       expect {
         get test_error_path
-      }.to raise_error(TestError)
+      }.to_not raise_error
     end
   end
 end
