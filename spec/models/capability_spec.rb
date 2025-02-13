795f0ec1df15bc9a3d33ced00bbc18069b42b180
Ed Davey <ed.davey@education.gov.uk>
2025-02-13 17:36:46 +0000

    Add 'find-or-creators' to Capability

--- spec/models/capability_spec.rb
+++ spec/models/capability_spec.rb
@@ -16,6 +16,7 @@ RSpec.describe Capability do
       expect(capability.errors.full_messages).to include("Description can't be blank")
     end
   end
+
   describe "enforces uniqueness of #name" do
     context "when a capability with the same name already exists" do
       before do
@@ -32,6 +33,119 @@ RSpec.describe Capability do
         )
 
         expect(new_capability.valid?).to be false
+      end
+    end
+  end
+
+  describe "capability finders" do
+    describe "::manage_team" do
+      context "when the team exists" do
+        let!(:existing_capability) do
+          Capability.create(
+            name: :manage_team,
+            description: "Capabilities dependent on the User#manage_team attribute"
+          )
+        end
+
+        it "returns the existing capability" do
+          expect(Capability.manage_team).to eq(existing_capability)
+        end
+      end
+
+      context "when the capability does not exist" do
+        it "creates the capability" do
+          allow(Capability).to receive(:find_or_create_by)
+
+          Capability.manage_team
+
+          expect(Capability).to have_received(:find_or_create_by)
+            .with(
+              name: :manage_team,
+              description: "Capabilities dependent on the User#manage_team attribute"
+            )
+        end
+
+        it "returns that newly created capability" do
+          newly_created_capability = Capability.manage_team
+
+          expect(newly_created_capability).to be_persisted
+          expect(newly_created_capability.name).to eq("manage_team")
+          expect(newly_created_capability.description).to match(/User#manage_team attribute/)
+        end
+      end
+    end
+
+    describe "::add_new_project" do
+      context "when the team exists" do
+        let!(:existing_capability) do
+          Capability.create(
+            name: :add_new_project,
+            description: "Capabilities dependent on the User#add_new_project attribute"
+          )
+        end
+
+        it "returns the existing capability" do
+          expect(Capability.add_new_project).to eq(existing_capability)
+        end
+      end
+
+      context "when the capability does not exist" do
+        it "creates the capability" do
+          allow(Capability).to receive(:find_or_create_by)
+
+          Capability.add_new_project
+
+          expect(Capability).to have_received(:find_or_create_by)
+            .with(
+              name: :add_new_project,
+              description: "Capabilities dependent on the User#add_new_project attribute"
+            )
+        end
+
+        it "returns that newly created capability" do
+          newly_created_capability = Capability.add_new_project
+
+          expect(newly_created_capability).to be_persisted
+          expect(newly_created_capability.name).to eq("add_new_project")
+          expect(newly_created_capability.description).to match(/User#add_new_project attribute/)
+        end
+      end
+    end
+
+    describe "::assign_to_project" do
+      context "when the team exists" do
+        let!(:existing_capability) do
+          Capability.create(
+            name: :assign_to_project,
+            description: "Capabilities dependent on the User#assign_to_project attribute"
+          )
+        end
+
+        it "returns the existing capability" do
+          expect(Capability.assign_to_project).to eq(existing_capability)
+        end
+      end
+
+      context "when the capability does not exist" do
+        it "creates the capability" do
+          allow(Capability).to receive(:find_or_create_by)
+
+          Capability.assign_to_project
+
+          expect(Capability).to have_received(:find_or_create_by)
+            .with(
+              name: :assign_to_project,
+              description: "Capabilities dependent on the User#assign_to_project attribute"
+            )
+        end
+
+        it "returns that newly created capability" do
+          newly_created_capability = Capability.assign_to_project
+
+          expect(newly_created_capability).to be_persisted
+          expect(newly_created_capability.name).to eq("assign_to_project")
+          expect(newly_created_capability.description).to match(/User#assign_to_project attribute/)
+        end
       end
     end
   end
