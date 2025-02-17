897bb9fa8e33486ca520fd14c61ca52e7b56e1cb
Ed Davey <ed.davey@education.gov.uk>
2025-02-17 11:22:59 +0000

    Describe existing behaviour: RCS managing team
    At present a "Regional casework services team member who is also a
    manager can not see the "Your projects" section.
    
    Before changing this behaviour we document it.

--- spec/features/your_projects/users_can_view_their_projects_spec.rb
+++ spec/features/your_projects/users_can_view_their_projects_spec.rb
@@ -39,6 +39,33 @@ RSpec.feature "Users can view their projects" do
     expect(page).to_not have_content(project.urn)
   end
 
+  context "when they ARE in the Regional casework services team" do
+    context "and they are NOT a team manager" do
+      before {
+        user.save # perform the on_save callbacks
+        expect(user.manage_team).to be false
+      }
+
+      it "they can access the 'Your projects' section" do
+        visit in_progress_your_projects_path
+        expect(page).to have_link("Your project")
+      end
+    end
+
+    context "and they ARE a team manager" do
+      before {
+        user.update_column(:manage_team, true)
+        user.save # perform the on_save callbacks
+        expect(user.reload.manage_team).to be true
+      }
+
+      it "they can NOT access the 'Your projects' section" do
+        visit "/"
+        expect(page).to_not have_link("Your project")
+      end
+    end
+  end
+
   context "when they are NOT in the Regional casework services team" do
     let(:user) { create(:regional_delivery_officer_user) }
 
