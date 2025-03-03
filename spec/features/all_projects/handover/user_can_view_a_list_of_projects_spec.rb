3aa72f795483a54654dcf3f4f65786f21309b738
Ed Davey <ed.davey@education.gov.uk>
2025-03-03 11:30:11 +0000

    Show 'find inactive project' when > 1 page of projects to handover
    If there is more than one page of projects to handover we now offer a
    "Search for inactive project by URN" feature.
    
    If a matching inactive project is found we provide a link to that
    project's "check handover details" page. If no matching inactive project
    exists we return the user to list of projects to be handed over.
    
    User story
    ---
    - So that I can review a particular project which is to be handed over
    - As a Regional Case Officer team leader at a time when there are over
      20 projects in the "to be handed over" (inactive) state
    - I want to locate this "to handover" (inactive) project without paging
      through multiple records

--- spec/features/all_projects/handover/user_can_view_a_list_of_projects_spec.rb
+++ spec/features/all_projects/handover/user_can_view_a_list_of_projects_spec.rb
@@ -26,4 +26,82 @@ RSpec.feature "Users can view a list of handover projects" do
       expect(page).to have_content(conversion_project.urn)
     end
   end
+
+  context "finding a particular inactive project" do
+    let(:max_projects_per_page) { Pagy::DEFAULT.fetch(:items) }
+
+    before do
+      max_projects_per_page.times { create(:conversion_project, state: :inactive) }
+    end
+
+    context "when all the _inactive_ projects can be shown on a single page" do
+      before do
+        visit root_path
+        click_link "All projects"
+        click_link "Handover"
+      end
+
+      scenario "there's no need to search for a particular _inactive_ project" do
+        expect(page).to_not have_content("Search for project to be handed over")
+      end
+    end
+
+    context "when NOT all the _inactive_ projects can be shown on a single page" do
+      let!(:target_project) { create(:conversion_project, state: :inactive, urn: 919191) }
+
+      let!(:active_project) { create(:conversion_project, state: :active, urn: 666333) }
+
+      before do
+        visit root_path
+        click_link "All projects"
+        click_link "Handover"
+      end
+
+      scenario "it's possible to search for a particular _inactive_ project" do
+        then_i_have_a_way_to_find_a_particular_inactive_project
+
+        when_i_search_for_a_particular_project_which_exists
+        then_i_find_that_particular_project
+        and_use_the_link_to_add_handover_details
+
+        when_i_search_for_a_particular_project_which_does_not_exist
+        then_i_see_a_helpful_message_and_can_search_again
+      end
+    end
+
+    def then_i_have_a_way_to_find_a_particular_inactive_project
+      expect(page).to have_field("Search for project to be handed over")
+    end
+
+    def when_i_search_for_a_particular_project_which_exists
+      within ".dfe-body__search" do
+        fill_in("Search by URN", with: target_project.urn)
+        click_button("Search")
+      end
+    end
+
+    def then_i_find_that_particular_project
+      expect(page).to have_content("A project to be handed over with URN #{target_project.urn} was found")
+      expect(page).to have_content(target_project.establishment.name)
+      expect(page).to_not have_content("Search for project to be handed over")
+    end
+
+    def and_use_the_link_to_add_handover_details
+      expect(page).to have_link("Add handover details", href: check_all_handover_projects_path(target_project))
+    end
+
+    def when_i_search_for_a_particular_project_which_does_not_exist
+      click_link "Handover"
+      within ".dfe-body__search" do
+        expect(page).to have_content("Search for project to be handed over")
+        fill_in("Search by URN", with: active_project.urn)
+        click_button("Search")
+      end
+    end
+
+    def then_i_see_a_helpful_message_and_can_search_again
+      expect(page).to have_content("No project to be handed over with URN #{active_project.urn} was found")
+      expect(page).to have_content("Search for project to be handed over")
+    end
+  end
 end
