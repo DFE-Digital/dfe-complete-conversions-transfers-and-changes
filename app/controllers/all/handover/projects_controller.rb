e69035f353d0ed4558065a3fee2140af377b6247
Ed Davey <ed.davey@education.gov.uk>
2025-03-03 11:28:00 +0000

    Add route and action to find inactive project by URN

--- app/controllers/all/handover/projects_controller.rb
+++ app/controllers/all/handover/projects_controller.rb
@@ -5,6 +5,21 @@ class All::Handover::ProjectsController < ApplicationController
     authorize Project, :handover?
 
     @pager, @projects = pagy(Project.inactive.ordered_by_significant_date)
+    @enable_search = @pager.pages > 1
     AcademiesApiPreFetcherService.new.call!(@projects)
+  end
+
+  def search
+    authorize Project, :handover?
+
+    @search_result = Project.inactive.find_by(urn: params[:urn_query])
+
+    if @search_result.blank?
+      @failed_search_urn = params[:urn_query]
+      flash[:error] = "No project to be handed over with URN #{params[:urn_query]} was found"
+      redirect_to action: :index
+    else
+      render :index
+    end
   end
 end
