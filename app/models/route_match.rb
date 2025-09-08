3811afd6a48c28c29215bea3b1277e94b1ca4c91
DEIFALLA <Mohamed.DEIFALLA@EDUCATION.GOV.UK>
2025-09-08 14:02:44 +0100

    update code

--- app/models/route_match.rb
+++ app/models/route_match.rb
@@ -10,9 +10,16 @@ class RouteMatch
     # Flatten and compact the patterns to ensure we have a simple array of strings
     flattened_patterns = @matching_patterns.flatten.compact.select { |p| p.is_a?(String) }
     
-    flattened_patterns.any? do |pattern|
-      glob_match?(route, pattern)
+    Rails.logger.debug "Checking route '#{route}' against patterns: #{flattened_patterns.inspect}"
+    
+    result = flattened_patterns.any? do |pattern|
+      matches = glob_match?(route, pattern)
+      Rails.logger.debug "Pattern '#{pattern}' matches route '#{route}': #{matches}"
+      matches
     end
+    
+    Rails.logger.debug "Route '#{route}' matches any pattern: #{result}"
+    result
   end
 
   private
@@ -30,6 +37,10 @@ class RouteMatch
     # Add anchors to match the entire path
     regex_pattern = "\\A#{regex_pattern}\\z"
     
-    path.match?(Regexp.new(regex_pattern))
+    Rails.logger.debug "Converting pattern '#{pattern}' to regex: '#{regex_pattern}'"
+    
+    result = path.match?(Regexp.new(regex_pattern))
+    Rails.logger.debug "Regex '#{regex_pattern}' matches path '#{path}': #{result}"
+    result
   end
 end
--- app/services/dotnet_rerouting_rules_service.rb
+++ app/services/dotnet_rerouting_rules_service.rb
@@ -25,25 +25,40 @@ class DotnetReroutingRulesService
   def extract_match_values(rule_json)
     return [] unless rule_json.is_a?(Hash)
 
+    Rails.logger.info "Extracting match values from rule: #{rule_json.inspect}"
+
     conditions =
       rule_json.dig('properties', 'conditions') ||
       rule_json.dig('properties', 'matchConditions') ||
       []
 
+    Rails.logger.info "Found #{conditions.length} conditions"
+
     request_uri_cond = conditions.find do |c|
       next false unless c.is_a?(Hash)
       
       name = (c['name'] || c['type'] || '').to_s
+      Rails.logger.info "Checking condition: #{name}"
       name.match?(/request\s*uri|requesturi|url\s*path/i)
     end
 
-    return [] unless request_uri_cond
+    unless request_uri_cond
+      Rails.logger.info "No request URI condition found"
+      return []
+    end
+
+    Rails.logger.info "Found request URI condition: #{request_uri_cond.inspect}"
 
     params = request_uri_cond['parameters'] || request_uri_cond['matchParameters']
-    return [] unless params.is_a?(Hash)
+    unless params.is_a?(Hash)
+      Rails.logger.info "No parameters found in condition"
+      return []
+    end
 
     values = params['matchValues'] || params['values']
-    Array(values).compact.select { |v| v.is_a?(String) }
+    result = Array(values).compact.select { |v| v.is_a?(String) }
+    Rails.logger.info "Extracted values: #{result.inspect}"
+    result
   end
 
   # rule_set_json: full rules response
@@ -54,14 +69,24 @@ class DotnetReroutingRulesService
     rules = rule_set_json.dig('value') || []
     Rails.logger.info "Found #{rules.length} rules in response"
     
-    # Filter for rules that contain "reroutingdotnet" in the name to get .NET rerouting patterns
+    # Filter for rules that contain "completedotnetreroute" in the name to get .NET rerouting patterns
     dotnet_rules = rules.select do |rule|
       rule_name = rule.dig('name') || ''
-      Rails.logger.info "Checking rule: #{rule_name}"
-      rule_name.to_s.downcase.include?('completedotnetreroute')
+      rule_set_name = rule.dig('properties', 'ruleSetName') || ''
+      Rails.logger.info "Checking rule: #{rule_name}, ruleSetName: #{rule_set_name}"
+      
+      # Check both rule name and rule set name for .NET rerouting
+      rule_name.to_s.downcase.include?('completedotnetreroute') ||
+      rule_set_name.to_s.downcase.include?('completedotnetreroute')
     end
     
     Rails.logger.info "Found #{dotnet_rules.length} .NET rerouting rules"
+    
+    # If no .NET specific rules found, try all rules as fallback
+    if dotnet_rules.empty?
+      Rails.logger.info "No .NET specific rules found, trying all rules as fallback"
+      dotnet_rules = rules
+    end
     
     # Ensure we get a flattened array of strings
     patterns = dotnet_rules.flat_map { |r| extract_match_values(r) }.flatten.compact.select { |p| p.is_a?(String) }.uniq
