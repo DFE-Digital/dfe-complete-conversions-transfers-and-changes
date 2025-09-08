4badfe4ca3af9a755e7f5f1ac3a474e62346db25
DEIFALLA <Mohamed.DEIFALLA@EDUCATION.GOV.UK>
2025-09-08 12:34:15 +0100

    fix routing

--- app/models/route_match.rb
+++ app/models/route_match.rb
@@ -11,7 +11,25 @@ class RouteMatch
     flattened_patterns = @matching_patterns.flatten.compact.select { |p| p.is_a?(String) }
     
     flattened_patterns.any? do |pattern|
-      route.match?(Regexp.new(Regexp.escape(pattern)))
+      glob_match?(route, pattern)
     end
+  end
+
+  private
+
+  def glob_match?(path, pattern)
+    # Convert glob pattern to regex
+    # * matches any characters except /
+    # ** matches any characters including /
+    # ? matches single character except /
+    regex_pattern = pattern
+      .gsub(/\*\*/, '.*')  # ** matches any characters including /
+      .gsub(/\*/, '[^/]*') # * matches any characters except /
+      .gsub(/\?/, '[^/]')  # ? matches single character except /
+    
+    # Add anchors to match the entire path
+    regex_pattern = "\\A#{regex_pattern}\\z"
+    
+    path.match?(Regexp.new(regex_pattern))
   end
 end
